1eec44e66a4beb4365159744ee3cde1c3114546e
YoussefMeziani <meziani.youssef@hotmail.com>
2024-09-24 23:27:59 +0200

    # 1.How many seconds are there in 42 minutes 42 seconds?
    https://colab.research.google.com/github/AllenDowney/ThinkPython/blob/v3/chapters/chap01.ipynb#scrollTo=9d562609

---
+++ Chap1_Type.py
@@ -0,0 +1,10 @@
+print("----------------------------------------------------")
+# 1.How many seconds are there in 42 minutes 42 seconds?
+print("1.How many seconds are there in 42 minutes 42 seconds?")
+minutes : 60
+seconds : 1
+minutes = 42
+seconds = 42
+output = (minutes * 60 + seconds)
+print(output, "seconds")
+
