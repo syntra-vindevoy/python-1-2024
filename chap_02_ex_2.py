592ec1ed0bbfa019051ffd647e9bef142c8f5a13
besa03 <zekaj.berat@hotmail.com>
2024-09-25 00:19:55 +0200

    solution exercise 1 of chapter 2

--- chap_02_ex_2.py
+++ chap_02_ex_2.py
@@ -1,12 +1,5 @@
 import math
 
-x = 5
+x = 42
 sum = (math.cos(x) **2 ) + (math.sin(x) **2 )
-print(f"the sum is {sum}")
-
-deviation = abs(sum - 1)
-
-if deviation < 0.000001:
-    print("Het is ok")
-else:
-    print(f"Het is niet ok want de uitkomst is {sum}")
+print(sum)
