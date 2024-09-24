6cfa5493ba3a0a9ed7988a1b73023f69d511e178
mvdholen <marijnvandenholen@gmail.com>
2024-09-24 20:46:28 +0200

    auto-format on save

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
-def get_seconds(minutes:int,seconds:int)->int:
-    seconds_from_minutes = minutes*60
+def get_seconds(minutes: int, seconds: int) -> int:
+    seconds_from_minutes = minutes * 60
     return seconds_from_minutes + seconds
 
 
-def get_miles_from_kilometers(kilometers:float)->float:
-    return kilometers*1.609
+def get_miles_from_kilometers(kilometers: float) -> float:
+    return kilometers * 1.609
 
 
-
-
-
-def is_leap_year(year:int)->bool:
+def is_leap_year(year: int) -> bool:
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
@@ -22,36 +19,45 @@ def is_leap_year(year:int)->bool:
     else:
         return False
 
-def is_leap_year_0(year:int)->bool:
+
+def is_leap_year_0(year: int) -> bool:
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
                 return True
             else:
                 return False
-        else: return True
+        else:
+            return True
     else:
         return False
 
-def is_leap_year_1(year:int)->bool:
+
+def is_leap_year_1(year: int) -> bool:
     if year % 4 > 0: return False
     if year % 100 > 0: return True
     if year % 400 > 0: return False
     return True
 
-def is_leap_year_2(year:int)->bool:
-    if year % 4 > 0: return False
-    elif year % 100 > 0: return True
-    elif year % 400 > 0: return False
+
+def is_leap_year_2(year: int) -> bool:
+    if year % 4 > 0:
+        return False
+    elif year % 100 > 0:
+        return True
+    elif year % 400 > 0:
+        return False
     return True
 
-def is_leap_year_3(year:int):
+
+def is_leap_year_3(year: int):
     return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
+
 def main():
-    years = (2000,2001,2002,2003, 2004)
+    years = (2000, 2001, 2002, 2003, 2004, 2100, 2200)
     for y in years:
-        print (f"year= {y}")
+        print(f"year= {y}")
         print(is_leap_year(y))
         print(is_leap_year_0(y))
         print(is_leap_year_1(y))
