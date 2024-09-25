e0daca3178500195140470642c1a95421d0b2ebb
bedoosd <teck_kenneth@hotmail.com>
2024-09-25 21:25:50 +0200

    begin

---
+++ Exercises chapter 2.py
@@ -0,0 +1,10 @@
+from math import pi, cos, sin
+
+radius = 5 #centimeters
+volume = 4/3*pi*(radius**3) #result in cubic centimeters
+print (f"the volume of the sphere with a {radius} cm radius is {volume} cm³")
+
+x = 42
+close_to_1 = (cos (x))**2 + (sin (x))**2
+print (close_to_1)
+
