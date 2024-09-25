baa6ada3ef91c22e545924e808be1a66ca2b15e3
chiel <chiel.vansompel@gmail.com>
2024-09-25 20:21:56 +0200

    Solved exercises for chapter 2

---
+++ exercises/Chapter_02.py
@@ -0,0 +1,18 @@
+import math
+
+radius = 5
+volume = ((4/3) * math.pi * radius ** 3)
+print(f"The volume with a radius of {radius} cm is {volume} cubic centimetres")
+
+x = 42
+val_cos = (math.cos(x)) ** 2
+val_sin = (math.sin(x)) ** 2
+val_sum = val_cos + val_sin
+print(f"the cos of {x} is {val_cos} and the sin of {x} is {val_sin}, the sum of both is {val_sum}")
+
+method1 = math.e ** 2
+print(method1)
+method2 = math.pow(math.e,2)
+print(method2)
+method3 = math.exp(2)
+print(method3)
