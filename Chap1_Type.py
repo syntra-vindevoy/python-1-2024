e92a4beda6cd8fec5387fad631867a1ca83227e5
YoussefMeziani <meziani.youssef@hotmail.com>
2024-09-25 19:31:55 +0200

    # 1.How many seconds are there in 42 minutes 42 seconds? # 2.How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile. # 3.If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace in seconds per mile? # 4.What is your average pace in minutes and seconds per mile? print("5.What is your average speed in miles per hour?")
    https://colab.research.google.com/github/AllenDowney/ThinkPython/blob/v3/chapters/chap01.ipynb#scrollTo=9d562609

--- Chap1_Type.py
+++ Chap1_Type.py
@@ -6,7 +6,7 @@ seconds : 1
 minutes = 42
 seconds = 42
 output = (minutes * 60 + seconds)
-print(output, "seconds")
+print(output, "seconds.")
 
 print("----------------------------------------------------")
 # 2.How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
@@ -15,24 +15,36 @@ kilometers : 1
 Miles : 1.61
 kilometers = 10
 Miles = 1.61
+
 output = (kilometers / Miles)
 output = round(output, 2)
-print(output," miles in 10 kilometers" )
+print(output," miles in 10 kilometers." )
 
 print("----------------------------------------------------")
 # 3.If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace in seconds per mile?
 print("3.If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace in seconds per mile?")
-
 hour : 1
-minutes = 42
-seconds = 42
 
 output = (minutes * 60 + seconds)
 output2 = (kilometers/Miles)
 output3 = (output2/output)
 output3= round(output3,4)
-print(output3,"mph average speed in hour")
+print(output3,"mph average speed in hour.")
 
+print("----------------------------------------------------")
+# 4.What is your average pace in minutes and seconds per mile?
+print("4.What is your average pace in minutes and seconds per mile?")
+output1= (minutes + seconds / 60)
+output3 = (output2/output1)
+output3= round(output3,4)
+print(output3,"mph average speed in minutes and seconds per mile.")
 
+print("-----------------------------------------------------")
+print("5.What is your average speed in miles per hour?")
+output1 = (minutes / 60 + seconds / 3600)
+output2 = (kilometers/Miles)
+output3 = (output2/output1)
+output3 = round(output3,2)
+print(output3,"mph average speed in hour.")
 
 
