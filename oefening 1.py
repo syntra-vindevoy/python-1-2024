5d97059efcd4de9145efb2999b7338876b242e5a
bedoosd <teck_kenneth@hotmail.com>
2024-09-25 22:09:13 +0200

    oefening 1 aangepast naar formatted strings

--- oefening 1.py
+++ oefening 1.py
@@ -1,16 +1,16 @@
 x=42
 y=42
 z= x*60+y
-print (x, "minutes +", y, "seconds=", z, "seconds")
+print (f"{x} minutes {y} seconds= {z} seconds")
 x=10
 y=x/1.61
-print ("there are", y, "miles in", x, "kilometers" )
+print (f"there are {y} miles in {x} kilometers")
 a=z/y
-print ("the pace is", a, "seconds per mile")
+print (f"the pace is {a} seconds per mile")
 b=a/60
 c=a-(int(b)*60)
-print ("that equals", int(b), "minutes, and", c, "seconds per mile")
-print ("or", int (b), "minutes,", int(c), "seconds and", round (((a-int(a))*100)), "hundreds")
+print (f"that equals {int(b)} minutes, and {c} seconds per mile")
+print (f"or {int (b)} minutes, {int(c)} seconds and {round (((a-int(a))*100))} hundreds")
 h=60*60
 i=z/h
-print ("the average speed is", i*y, "miles per hour")
+print (f"the average speed is {i*y} miles per hour, ({round((i*y)*100)/100})miles per hour")
