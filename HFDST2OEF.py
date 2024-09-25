93369dd5abea6feb96f2aad2d5f7fe337046ddd3
Freekvera <veraghtertfreek@gmail.com>
2024-09-25 21:50:49 +0200

    Oefeningen hfdst2 en card draw calculator

---
+++ HFDST2OEF.py
@@ -0,0 +1,16 @@
+import math
+
+radius = 5
+
+volume = (4/3) * math.pi * (radius ** 3)
+print(f"The volume of the sphere is {volume} cubic centimeters.")
+
+x = 42
+result1 = math.cos(x)**2+math.sin(x)**2
+print(f"The result of this calculation is {result1}.")
+
+y = math.e**2
+z = math.exp(2)
+
+print(f"math.e**2 is {y}.")
+print(f"math.exp(2) is {z}.")
