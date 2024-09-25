bd270a29f6f007f9a018a19095d49fec6ec526fe
bertje985 <bert_geeroms@hotmail.com>
2024-09-26 00:10:37 +0200

    commit 2.3 final

---
+++ chapter02/oef03.py
@@ -0,0 +1,11 @@
+import math
+
+#part 1
+e_expo_1 = math.e**1
+print(e_expo_1)
+
+#part 2
+print(math.pow(math.e, 2))
+
+#part 3  the value u use in exp is the exponent is used to calculate ex
+print(math.exp(3))
