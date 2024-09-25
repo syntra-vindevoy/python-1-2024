9eea85e3680038b2da87c9a973b41638918cac89
Thomas <thomas.meersschaut@gmail.com>
2024-09-25 19:39:25 +0200

    Chap01_Exe

---
+++ chap01_exe.py
@@ -0,0 +1,53 @@
+#Chapter 01: Exercises
+import math
+
+print(25.8 / 2)
+print(25.8 // 2)        #Integer div is always flooring down
+print("#" * 50)
+
+print(round(21.4))      #When < .5 --> rounding down
+print(round(21.6))      #When > .5 --> rounding up
+
+                        #When == .5
+print(round(21.5))      #Both give 22 --> round of .5 will go to the nearest even number
+print(round(22.5))
+print("#" * 50)
+
+pi = math.pi
+print(pi)
+
+round_pi = round(pi)
+print(round_pi)
+round_pi = round(pi, 3)
+print(round_pi)
+print("#" * 50)
+
+print(type(765))
+print(type(2.718))
+print(type('2 pi'))
+print(type(abs(-7)))
+print(type(abs(-7.0)))
+print(type(abs))
+print(type(int))
+print(type(type))
+print("#" * 50)
+
+
+
+#Excersie 1.9.5
+sec = (42 * 60) + 42
+print(sec)
+
+miles = (10 / 1.61)
+print(miles)
+
+sec_mile = sec / miles
+print(sec_mile)
+
+min_mile = sec_mile // 60
+rem_sec_mile = sec_mile % 60
+print(min_mile)
+print(rem_sec_mile)
+
+miles_h = (miles / sec) * 3600
+print(miles_h)
--- chap01_type.py
+++ chap01_type.py
@@ -25,21 +25,3 @@ print(type(b) == A)         #type doesn't take into account inheritance
 print(isinstance(b, A))     #isinstance takes into account inheritance --> usage of isinstance is better!!!!
 
 print(type((4,2)))
-
-#Excersie 1.9.5
-time = (42 * 60) + 42
-print(time)
-
-mile = (10 / 1.61)
-print(mile)
-
-sec_per_mile = time / mile
-print(sec_per_mile)
-
-min_per_mile = sec_per_mile // 60
-rem_sec_mile = sec_per_mile % 60
-print(min_per_mile)
-print(rem_sec_mile)
-
-miles_h = (mile / time) * 3600
-print(miles_h)
