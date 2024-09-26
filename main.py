79237ff65ef0df4fe8d64dae730b6aa9ff2a90f7
benoit <benoit.goethals@gmail.com>
2024-09-26 07:33:18 +0200

    update

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import turtle
+
+def rectangle(height, width_rec,star):
+    for i in range(2):
+        star.forward(width_rec)
+        star.left(90)
+        star.forward(height)
+        star.left(90)
+
 def main():
-    print("Hello Benoit")
-    print(abs(10.3))
-    # bad example
-    aéss = lo = "dsfds"
-    print(aéss)
+
+    star = turtle.Turtle()
+    rectangle(80, 40, star)
+    turtle.done()
+
+
+
+
 if __name__ == '__main__':
     main()
