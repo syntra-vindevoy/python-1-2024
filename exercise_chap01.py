b6c5271aa9ef972881173e3c4248199104d75808
GeoffreyCrapoen <crapoen.geoffrey@gmail.com>
2024-09-26 13:21:29 +0200

    Refactor chapter exercises.

--- exercise_chap01.py
+++
@@ -1,3 +0,0 @@
-minutes = 42
-seconds = 42
-print (minutes * 60 + seconds)
---
+++ exercise_chap02.py
@@ -0,0 +1,11 @@
+import math
+
+radius = 5
+volume = int ((4/3) * math.pi * radius**3)
+x = 42
+formule = ((math.cos(x)**2) + (math.sin(x)**2) == 1 )
+print (f"het volume van de cirkel bedraagt: {volume} m³")
+print (formule)
+print (math.e ** 2)
+print (math.pow(math.e, 2))
+print (math.exp(2))
