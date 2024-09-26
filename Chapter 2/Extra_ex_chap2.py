0417994048672938d1cdffa488037b8b1128bb63
Synix1997 <deruyck.sebastien97@gmail.com>
2024-09-26 21:21:57 +0200

    Add dollar sign to result of wholesale cost

--- Chapter 2/Extra_ex_chap2.py
+++ Chapter 2/Extra_ex_chap2.py
@@ -7,5 +7,5 @@ store_price = book_price - (book_price * 0.4)
 copies = 60
 shipping_cost = 3 + ((copies - 1) * 0.75)
 total_cost = (copies * store_price) + shipping_cost
-print(f"The wholesale cost for 60 copies is {total_cost:.2f}")
+print(f"The wholesale cost for 60 copies is ${total_cost:.2f}")
 
