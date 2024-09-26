40dff5d5faefe1a8554ab1325ee6c2315492a1e0
besa03 <zekaj.berat@hotmail.com>
2024-09-26 21:07:36 +0200

    solution exercise 2.2 of chapter 2 #hardworkpaysoff

--- chap_02_ex_2_2.py
+++ chap_02_ex_2_2.py
@@ -4,18 +4,19 @@ price_book = 24.95
 number_of_copies = 60
 discount = 40/100
 
-shippingcost_first = 3 #eerste aan 3$
+shippingcost_first = 3 #eerste aan 3
 shippingcost_next = 0.75 #al de rest aan 0.75cent
 
 bookdiscount = (price_book * discount)
 bookafterdiscount = (price_book - bookdiscount)
+print(bookafterdiscount)
 
 price_first = bookafterdiscount+ shippingcost_first
 price_rest = (bookafterdiscount + shippingcost_next) * 59
 result = price_first + price_rest
 print(result)
 
-print(round(result, 2))
+
 
 
 
