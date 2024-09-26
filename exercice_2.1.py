05285ab2d520ec062f0e2aa17f46bb3bb4c34ffc
piet <pietvandewalle19@gmail.com>
2024-09-26 16:08:09 +0200

    commit 2.1

---
+++ exercice_2.1.py
@@ -0,0 +1,8 @@
+import math
+# Radius in cm
+radius = 5
+# In cubic centimeters
+volume = 4 / 3 * math.pi * radius ** 3
+
+print(f"The volume of the radius {radius} is {volume:.2f} cubic centimeters")
+
