8f95f745c6aeb350140b2f9e09a45cb5e41ef780
Synix1997 <deruyck.sebastien97@gmail.com>
2024-09-26 20:50:23 +0200

    Chapter 2 - Extra ex

--- Extra_ex_chap2.py
+++ Extra_ex_chap2.py
@@ -1,3 +1,7 @@
+#Suppose the cover price of a book is $24.95, but bookstores get a 40% discount.
+#Shipping costs $3 for the first copy and 75 cents for each additional copy.
+#What is the total wholesale cost for 60 copies?
+
 book_price = 24.95
 store_price = book_price - (book_price * 0.4)
 copies = 60
