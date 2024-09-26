ed477417dcd107728154d949534cb3e174137e20
besa03 <zekaj.berat@hotmail.com>
2024-09-26 20:01:26 +0200

    solution exercise 2 of chapter 2

--- chap_02_ex_2.py
+++ chap_02_ex_2.py
@@ -1,6 +1,10 @@
 import math
 
-x = 42
+x = 5
 sum = (math.cos(x) **2 ) + (math.sin(x) **2 )
 print(f"the sum is {sum}")
 
+deviation = abs(sum - 1)
+
+if deviation < 0.000001:
+    print(f"The sum is {sum}")
