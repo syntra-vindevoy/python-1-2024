e9cf650f586bf7ab225bd22743f7c08e26655f81
Synix1997 <deruyck.sebastien97@gmail.com>
2024-09-26 18:25:44 +0200

    Chapter 2 - exercises finished

---
+++ Chapter2_Ex.py
@@ -0,0 +1,20 @@
+# Part 1. The volume of a sphere with radius r is 4/3 * pi * r^3 .
+import math
+
+radius = 5
+#radius is in centimeters
+volume_sphere = radius * (4 / 3) * math.pi * (radius ^ 3)
+#volume is in cubic centimeters
+print(volume_sphere)
+
+# Part 2
+x = 42
+sine = math.sin(x)
+cosine = math.cos(x)
+print((sine**2)+(cosine**2))
+
+# Part 3
+e = math.e
+print(e ** 2)
+print(math.pow(e , 2))
+print(math.exp(2))
