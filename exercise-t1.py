155e414ce98466fabb0cf81306eb0e4e8c1a79eb
Theodoro99 <toufik_ammari@hotmail.com>
2024-09-26 12:13:22 +0200

    Practiced using Python calculator

---
+++ exercise-t1.py
@@ -0,0 +1,19 @@
+import math
+radius = 5
+volume = (4/3) * math.pi * radius**3
+print(f"The volume of a sphere with radius {radius} cm is {volume:.2f} cm³")
+ ###########
+
+import math
+x = 42
+sin_x = math.sin(x)
+cos_x = math.cos(x)
+sum_of_squares = sin_x**2 + cos_x**2
+print(f"For x = {x}, (cos(x))^2 + (sin(x))^2 = {sum_of_squares:.10f}")
+
+###########
+
+import math
+result = math.e ** 2
+print(result)
+
