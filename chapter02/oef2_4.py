3567fc825b457246d315f17d1f5800e29cd6efee
bertje985 <bert_geeroms@hotmail.com>
2024-09-26 20:51:49 +0200

    commit 2.4 final

---
+++ chapter02/oef2_4.py
@@ -0,0 +1,13 @@
+unit_price = 24.95
+discount = unit_price * 40/100
+first_shipment = 3
+later_shipment = 0.75
+nbr_copies = 60
+
+price_with_discount = round(unit_price - discount, 2)
+
+shipment_costs = first_shipment + ((nbr_copies - 1) * later_shipment)
+
+total_cost = (nbr_copies * price_with_discount) + shipment_costs
+
+print(total_cost)
