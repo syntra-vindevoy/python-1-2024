2eaf338694c757c437296ff40fbef5cd6cad5ec8
Synix1997 <deruyck.sebastien97@gmail.com>
2024-09-26 20:06:49 +0200

    Chapter 3 - Trying chapt 3 myself

---
+++ Chapter3_class.py
@@ -0,0 +1,15 @@
+# Class exercises for Chapter 3
+import string
+
+
+def print_lyrics():
+    print("Everywhere we go-oh")
+    print("People want to kno-ow")
+
+print_lyrics()
+
+def print_twice(string):
+    print(string)
+    print(string)
+
+print_twice("YoYo")
