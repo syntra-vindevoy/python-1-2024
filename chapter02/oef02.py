432db75002aadfbf9d0ae3935d0ec7301aa6702f
bertje985 <bert_geeroms@hotmail.com>
2024-09-26 20:13:23 +0200

    commit 2.3 final

--- chapter02/oef02.py
+++ chapter02/oef02.py
@@ -1,6 +1,9 @@
 import math
 
-x = 42
+x = 5
+
+y = 10 ** -5
+
 
 sum_rule = (math.cos(x))**2 + (math.sin(x))**2
 
@@ -8,3 +11,13 @@ if sum_rule == 1:
     print("true")
 else:
     print("false")
+
+
+    #oplossing om af te ronden van Yves. (er kon ook round gebruikt worden, zeker in dit geval), maar deviation is beter.
+
+deviation = abs(sum_rule - 1)
+if deviation < 10 ** -5:  #10 ** -5 10 tot de macht -5. geeft als waarde 0.000005, gemakkelijker ipv het getal te moeten typen en te gaan tellen hoeveel x 0
+    print("true")
+else:
+    print("false")
+
--- chapter02/oef03.py
+++ chapter02/oef03.py
@@ -7,5 +7,10 @@ print(e_expo_1)
 #part 2
 print(math.pow(math.e, 2))
 
-#part 3  the value u use in exp is the exponent is used to calculate ex
+#part 3  the value u use in exp is the exponent is used to calculate e tot de macht x
 print(math.exp(3))
+
+
+# oefening in de les (voor part 2)
+x = 2
+print(math.pow(math.e, x))
