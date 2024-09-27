be6ab34757d3d5bceca130a462439d1c16d166e1
bucofski <tom_vandevyver@telenet.be>
2024-09-27 10:56:38 +0200

    Last exercise made. Found the calculation fault.

--- Ex2.py
+++ Ex2.py
@@ -1,6 +1,6 @@
 n = 17
 # 17 = n ==> syntax error
-x = y = 1
+a = y = 1
 z = 2;
 # e = 3. syntax error
 # import maath ==> syntax error
@@ -20,7 +20,7 @@ print("The volume of the sphere is:", volume, "cubic centimeters")
 # part 2
 
 # Define the value of x in radians
-x = 42  # x in radians
+x = 5  # x in radians
 
 # Compute cosine and sine of x
 cos_x = math.cos(x)
@@ -28,9 +28,23 @@ sin_x = math.sin(x)
 
 # Calculate the sum of their squares
 result = cos_x**2 + sin_x**2
-
+result = round(result, 1)
+deviation = abs(result -1)
 # Display the result
 print(f"cos^2(x) + sin^2(x) = {result}")
+
+#om te weten als de uitkomst waar is.
+if deviation < (10 ** -5):
+    print("het is ok")
+else :
+    print(f"het is niet in orde, want de uitkomst is {result} ")
+
+
+# if result == 1:
+#     print("het is ok")
+# else :
+#     print(f"het is niet in orde, want de uitkomst is {result} ")
+
 
 # part 3
 
@@ -47,3 +61,18 @@ e_squared_3 = math.exp(2)
 print(f"result with math.e ** 2: {e_squared_1}")
 print(f"result with math.pow: {e_squared_2}")
 print(f"result with math.exp: {e_squared_3}")
+
+price = 24.95
+discount = 40 / 100
+first_book_chip = 3
+rest_book_chip = 0.75
+amount_books = 10000
+discount_price = price * (1 - discount)
+print(discount_price)
+totalcost = ((discount_price * amount_books) + (rest_book_chip * (amount_books - 1)) + first_book_chip)
+rounded = round(totalcost, 2)
+
+
+print(f"totale prijs om een book te kopen: ${rounded} waarvan ")
+
+
---
+++ ex_2_last.py
@@ -0,0 +1,47 @@
+# easy_pace = ((8 * 60) + 15) * 2
+# four_miles = ((7 * 60) + 12) * 3
+#
+# total_time_sec = easy_pace + four_miles
+#
+# time_mid_hour_sec = 6 * 3600
+# time_mid_min_sec = 52 * 60
+#
+# real_time_sec = total_time_sec + time_mid_hour_sec + time_mid_min_sec
+#
+# hour = real_time_sec // 3600
+# minutes = (real_time_sec % 3600) // 60
+# seconds = real_time_sec % 60
+#
+# print (f"Ik arriveer thuis om {hour}:{minutes} en {seconds} seconden")
+
+easy_pace_min = 8
+easy_pace_sec = 15
+tempo_run_min = 7
+tempo_run_sec = 12
+amount_tempo_miles = 3
+amount_easy_miles = 2
+sec_in_minutes = 60
+sec_in_hours = 3600
+start_hour = 6
+start_minute = 52
+
+#total running time
+tot_easy_pace_sec = ((easy_pace_min * sec_in_minutes) + easy_pace_sec) * amount_easy_miles
+tot_tempo_sec = ((tempo_run_min * sec_in_minutes) + tempo_run_sec) * amount_tempo_miles
+
+#total run time in seconds
+total_time_sec = tot_easy_pace_sec + tot_tempo_sec
+
+#calculate sec till midnight
+time_mid_hour_sec = start_hour * sec_in_hours
+time_mid_min_sec = start_minute * sec_in_minutes
+
+#everthing in seconds till midnight
+real_time_sec = total_time_sec + time_mid_hour_sec + time_mid_min_sec
+
+# calculate the time in from total seconds till midnight
+hour = real_time_sec // sec_in_hours
+minutes = (real_time_sec % sec_in_hours) // sec_in_minutes
+seconds = real_time_sec % sec_in_minutes
+
+print (f"Ik arriveer thuis om {hour}:{minutes} en {seconds} seconden")
