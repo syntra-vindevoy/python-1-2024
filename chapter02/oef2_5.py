d2c350613138961509a69310e700c9ff3996c138
bertje985 <bert_geeroms@hotmail.com>
2024-09-30 16:18:34 +0200

    commit 2.5 final version + import logic

--- chapter02/oef2_5.py
+++ chapter02/oef2_5.py
@@ -1,28 +1,15 @@
 import datetime
-start_hour = 6
-start_minute = 52
-fastpace_sec = (7 * 60) + 12
-slowpace_sec = (8 * 60) + 15
-slow_laps = 2
-fast_laps = 3
 
-start_hour_sec = (6 * 3600) + (52*60)
-finish_sec = start_hour_sec + (slow_laps * slowpace_sec) + (fast_laps * fastpace_sec)
+start = ((6 * 3600) + 52 * 60) #start tijd is 6u52
+slow_lap = (8 * 60) + 15
+fast_lap = (7 * 60) + 12
+finish = start + (slow_lap * 2) + (fast_lap * 3)
+finish_hour = finish // 3600
+hour_remains = finish % 3600
+finish_minute = hour_remains // 60
+finish_sec = hour_remains % 60
 
-finish_hour = finish_sec / 3600
-remainder = finish_sec % 3600
-finish_minute = remainder / 60
-finish_sec = remainder % 3600
+print(f"Ik ben aangekomen om {finish_hour}u {finish_minute}min {finish_sec}sec")
 
-
-print(start_hour_sec)
-print(fastpace_sec)
-print(slowpace_sec)
-print(finish_sec)
-print(finish_hour)
-print(finish_minute)
-print(finish_sec)
-
-
------------
-
+finish_with_import = datetime.datetime.fromtimestamp(finish, datetime.UTC).strftime('%H:%M:%S')
+print(f"met een import komen we aan het zelfde resultaat: {finish_with_import}")
