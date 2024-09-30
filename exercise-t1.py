f4dda1d3de4e36190fa226c9ac59a56c2a996171
Theodoro99 <toufik_ammari@hotmail.com>
2024-09-30 20:06:30 +0200

    Practiced using Python calculator

--- exercise-t1.py
+++ exercise-t1.py
@@ -17,3 +17,30 @@ import math
 result = math.e ** 2
 print(result)
 
+############
+cover_price = 24.95
+discount = 0.40
+num_copies = 60
+first_copy_shipping_cost = 3
+additional_copy_shipping_cost = 0.75
+
+discounted_price = cover_price * (1 - discount)
+total_book_cost = num_copies * discounted_price
+total_shipping_cost = first_copy_shipping_cost + (num_copies - 1) * additional_copy_shipping_cost
+total_wholesale_cost = total_book_cost + total_shipping_cost
+
+print(f"The total wholesale cost for {num_copies} copies is ${total_wholesale_cost:.2f}")
+###################
+import datetime
+
+start_time = datetime.datetime(2023, 1, 1, 6, 52)
+
+easy_pace = datetime.timedelta(minutes=8, seconds=15)
+tempo_pace = datetime.timedelta(minutes=7, seconds=12)
+
+total_time = easy_pace + (tempo_pace * 3) + easy_pace
+
+end_time = start_time + total_time
+
+print(f"You'll get home at {end_time.strftime('%I:%M %p')}")
+
