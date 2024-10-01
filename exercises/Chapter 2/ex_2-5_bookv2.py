425ca361a973813cbe229722b7185c973bff612f
chiel <chiel.vansompel@gmail.com>
2024-10-01 19:23:16 +0200

    added additional solution using datetime library.

--- exercises/Chapter 2/ex_2-5_bookv2.py
+++ exercises/Chapter 2/ex_2-5_bookv2.py
@@ -9,3 +9,15 @@ hours = total_time // 3600
 minutes = (total_time - hours * 3600) // 60
 seconds = total_time - hours * 3600 - minutes * 60
 print(f"ik kom thuis om {hours}:{minutes} and {seconds} seconds")
+
+#alternatief met library
+
+import datetime
+
+start = datetime.timedelta(hours=6, minutes=52)
+slow = datetime.timedelta(minutes=8, seconds=15)
+fast = datetime.timedelta(minutes=7, seconds=12)
+
+print(start + slow * 2 + fast * 3)
+two_hours_later = datetime.timedelta(hours=2)
+print(datetime.datetime.now() + two_hours_later)
