e42b8e3d8a3f685a34375b881be8f559d41dc30f
bedoosd <teck_kenneth@hotmail.com>
2024-10-01 21:16:22 +0200

    begin

--- chapter 3/Chapter 3.py
+++ chapter 3/Chapter 3.py
@@ -14,3 +14,31 @@ print(type(hallo))
 def print_t(text):
     print(text)
 print_t("hallokes")
+
+def repeat (word, n):
+    print(word * n)
+repeat("what ", 10)
+
+for i in range(10):
+    print(i)
+for i in range(1, 24, 3):
+    print(i)
+for i in range(50, -1, -5):
+    print(i)
+
+def add(x, y):
+    a = x + y
+
+a = 10
+print(a)
+
+add(2, 3)
+
+print(a)  # geeft steeds de uitkomst van 10 omdat a = x + y uit een functie komt.
+
+assert (add(2, 3) == 5)
+
+if __name__ == "__main__":
+    print (__name__)
+
+
