b79b08c5f10076506d5918083179f5c17bc77464
Thomas <thomas.meersschaut@gmail.com>
2024-10-01 20:47:45 +0200

    Chap03: code added

--- chap03_func.py
+++ chap03_func.py
@@ -1,7 +1,43 @@
 #Functions
 
-def myfirstfunc():
+def my_greeting():
     print("Hello")
-    print("World")
 
-myfirstfunc()
+print(type(my_greeting))
+my_greeting()
+
+def printing(text):
+    print(text)
+
+printing("Hello World")
+printing(1 + 1)
+
+def calc_area(b, h):
+    return b * h
+
+print(calc_area(5, 10))
+
+
+for i in range(2):
+    print(i)
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 11, 2):       #1 = start point / 2 = until (but not included) / 3 = add i with this number
+    print(i)
+
+#############################################################################
+def add(x, y):
+    a = x + y                   #'a' is local just within the function
+
+    print(a)
+
+a = 100
+
+print(a)
+
+add(2, 3)
+
+print(a)
+
