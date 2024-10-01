125e148b393da3f27d1fa0d519d620495eb07a45
chiel <chiel.vansompel@gmail.com>
2024-10-01 19:23:48 +0200

    added additional solution using datetime library.

--- exercises/Chapter 2/ex_2-5_bookv2.py
+++ exercises/Chapter 2/ex_2-5_bookv2.py
@@ -19,5 +19,6 @@ slow = datetime.timedelta(minutes=8, seconds=15)
 fast = datetime.timedelta(minutes=7, seconds=12)
 
 print(start + slow * 2 + fast * 3)
+
 two_hours_later = datetime.timedelta(hours=2)
 print(datetime.datetime.now() + two_hours_later)
