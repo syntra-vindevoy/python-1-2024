5c99e6a4decafe589ec0eee8dd23c55ae9b15d06
bedoosd <teck_kenneth@hotmail.com>
2024-10-01 18:43:29 +0200

    mappen gemaakt

--- chapter 2/Exercises chapter 2.py
+++ chapter 2/Exercises chapter 2.py
@@ -7,9 +7,15 @@ print (f"the volume of the sphere with a {radius} cm radius is {volume} cm³")
 x = 42
 close_to_1 = (math.cos(x)) ** 2 + (math.sin(x)) ** 2
 print (close_to_1)
-
+deviation = abs(close_to_1 - 1)
+if deviation < 0.00000001:
+    print("het is oke")
+if deviation <10**-5:
+    print("dit is ook oke")
 print (math.e **2)
 resultaat = math.pow(math.e,2)
 print (resultaat)
 print (math.pow(math.e,2))
 print (math.exp(2))
+
+print (float(10**-8))
---
+++ chapter 3/Chapter 3.py
@@ -0,0 +1,18 @@
+def print_lyrics():
+    print("hello")
+    print("world")
+print_lyrics()
+
+def print_twice(string):
+    print (string)
+    print (string)
+
+print_twice("hallo")
+
+line = 'Dennis Moore, '
+print_twice(line)
+
+def repeat (word, n):
+    print(word*n)
+repeat("What?", 3)
+print (10**-5)
