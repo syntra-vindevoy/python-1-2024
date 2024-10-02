e71ef4220ad93aac909f3e6b0aceda18da6ea079
BenoitGoethals <benoit.goethals@gmail.com>
2024-10-02 11:28:37 +0200

    fix ex3 Grid

--- chapter3/functions_demo.py
+++ chapter3/functions_demo.py
@@ -4,6 +4,11 @@ def calculate_gcd (a:int, b:int) -> int:
         a, b = b, a % b
     return a
 
+def calculate_gcd_alt(a, b):
+    if b == 0:  # Base case: if b is 0, gcd is a
+        return a
+    else:
+        return calculate_gcd(b, a % b)
 
 assert calculate_gcd (2, 3) == 1,"Oh no! This assertion failed!"
 assert calculate_gcd (48, 12) == 12,"Oh no! This assertion failed!"
