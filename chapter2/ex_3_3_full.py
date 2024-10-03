3b6b2afa41930c957c42106fb3923934a2b54ee5
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-10-03 22:00:50 +0200

    Add multiple exercises for drawing grid patterns
    This commit includes several Python exercises focused on drawing grid patterns of varying complexities. Each exercise demonstrates different approaches to printing horizontal and vertical lines, as well as handling dynamic grid sizes. Example usage and testing functions are included where applicable.
    
    (cherry picked from commit 702b573908c54fa35ee9140ed610f5f7323fb4d7)
    
    # Conflicts:
    #	chapter2/ex_3_3_chatgtp.py
    #	chapter2/ex_3_3_full.py
    #	chapter2/ex_3_3_v1.py
    #	chapter2/ex_3_3_v2.py
    #	chapter2/ex_3_3_v3.py
    #	chapter2/ex_3_print_right.py
    #	chapter2/ex_3_triangle.py

---
+++ chapter2/ex_3_3_full.py
@@ -0,0 +1,25 @@
+def edge_line(horizontal, size):
+    r = "+"
+    r += ("-" * size + "+") * horizontal
+
+    return r
+
+
+def vol_line(horizontal, size):
+    r = "|"
+    r += (" " * size + "|") * horizontal
+
+    return r
+
+
+def squares(horizontal, vertical, size):
+    for _ in range(vertical):
+        print(edge_line(horizontal, size))
+
+        for _ in range(size):
+            print(vol_line(horizontal, size))
+
+    print(edge_line(horizontal, size))
+
+
+squares(5, 4, 3)
