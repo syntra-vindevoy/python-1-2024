4608a1de77566fbd4ff47b333aa2eaf72a22936a
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-03 19:34:15 +0200

    Ex 3.11.4

---
+++ Chapter 3/Chapter3_Ex 3.11.4.py
@@ -0,0 +1,9 @@
+# Write a function called rectangle that takes a string
+# and two integers and draws a rectangle with the given width and height, made up using copies of the string.
+# Here’s an example of a rectangle with width 5 and height 4, made up of the string 'H'.
+
+def rectangle(char , height , width):
+    for i in range(height):
+        print(char * width)
+
+rectangle('*',4,5)
