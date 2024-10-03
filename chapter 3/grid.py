b8311a6c17ca3a1dfe3b459beebe3e184cf30153
bedoosd <teck_kenneth@hotmail.com>
2024-10-03 20:56:17 +0200

    begin

---
+++ chapter 3/grid.py
@@ -0,0 +1,21 @@
+
+def line_1():
+    part_1 = "+ " + 4 * "- "
+    print (part_1 * 2 + "+")
+def line_2():
+    part_2 = f"| {(8 * " ")}"
+    print (part_2 * 3)
+def grid_halve ():
+    line_1()
+    for i in range (1, 5):
+        line_2()
+def grid():
+    grid_halve()
+    grid_halve()
+    line_1()
+
+grid()
+
+
+
+
