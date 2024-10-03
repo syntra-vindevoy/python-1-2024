2c284789163ded7f4f4cc070b5948edc84c4a140
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-03 19:45:03 +0200

    Removed wrong code - previous exercise

--- Chapter 3/Chapter3_Ex 3.11.4.py
+++ Chapter 3/Chapter3_Ex 3.11.4.py
@@ -2,17 +2,12 @@
 # and two integers and draws a rectangle with the given width and height, made up using copies of the string.
 # Here’s an example of a rectangle with width 5 and height 4, made up of the string 'H'.
 
-# def rectangle(char , height , width):
-#     for i in range(height):
-#         print(char * width)
-#
-# rectangle('*',4,5)
+def rectangle(char , height , width):
+    for i in range(height):
+         print(char * width)
 
-# OR
-def triangle(char, number):
-    for i in range(1 , number + 1):
-        print(char * i)
+rectangle('*',10,15)
 
-triangle("*" , 5)
+
 
 
