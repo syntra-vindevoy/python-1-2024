b712755274adfb845f0e7503ffabd5fcf6cda53d
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-03 19:43:32 +0200

    Added second option for more efficient result

--- Chapter 3/Chapter3_Ex 3.11.4.py
+++ Chapter 3/Chapter3_Ex 3.11.4.py
@@ -2,8 +2,17 @@
 # and two integers and draws a rectangle with the given width and height, made up using copies of the string.
 # Here’s an example of a rectangle with width 5 and height 4, made up of the string 'H'.
 
-def rectangle(char , height , width):
-    for i in range(height):
-        print(char * width)
+# def rectangle(char , height , width):
+#     for i in range(height):
+#         print(char * width)
+#
+# rectangle('*',4,5)
 
-rectangle('*',4,5)
+# OR
+def triangle(char, number):
+    for i in range(1 , number + 1):
+        print(char * i)
+
+triangle("*" , 5)
+
+
