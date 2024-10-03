3195894b357ab13658d1a404bdeff8ba6c4a547e
Thomas <thomas.meersschaut@gmail.com>
2024-10-03 21:44:18 +0200

    chap03_ex8: comments added

--- chap03/chap03_ex8.py
+++ chap03/chap03_ex8.py
@@ -1,8 +1,8 @@
 #Chapter 3 - Exercise 8
-def square(x, y, z, f, g):
-    for i in range(y):
-        f(x, z)
-        for j in range(z):
+def square(x, y, z, f, g):              #Another option would have been to define functions pattern1 & 2 inside the function square
+    for _ in range(y):                  #Similar like a variable who's declared inside a function, the def in a def is local
+        f(x, z)                         #Technically it was not necessary to add "f" and "g" as parameter to def square
+        for _ in range(z):              #def pattern1 & 2 could just be called in the def square without it being in the parameters
             g(x, z)
     f(x, z)
 
