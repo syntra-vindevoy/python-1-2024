262b4935cd4524940a36a7f5e3d321a51cdd5650
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-03 19:45:59 +0200

    Added more efficient code for same result

--- Chapter 3/Chapter3_Ex 3.11.3.py
+++ Chapter 3/Chapter3_Ex 3.11.3.py
@@ -2,8 +2,15 @@
 # draws a pyramid with the given height, made up using copies of the string.
 # Here’s an example of a pyramid with 5 levels, using the string 'L'.
 
-def triangle(text , height):
-    for i in range(height):
-        print(text * (i + 1))
+# def triangle(text , height):
+#     for i in range(height):
+#         print(text * (i + 1))
+#
+# triangle("L" , 5)
 
-triangle("L" , 5)
+# OR
+def triangle(char, number):
+    for i in range(1 , number + 1):
+        print(char * i)
+
+triangle("*" , 5)
