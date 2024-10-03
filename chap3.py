b7e5c3c9b8398b3be5144263a2bf42c833dbde37
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-10-03 18:33:33 +0200

    Add new chapter 3 files and math support utilities
    Introduce five new files: chap3.py, chap3_global.py, global_strings.py, my_math.py, and chap3_dir.py. These additions provide utility functions for basic arithmetic and string management, as well as a factorial calculation with validation and error handling.

---
+++ chap3.py
@@ -0,0 +1,22 @@
+# def print_t(text):
+#     print(text)
+#
+# print_t("toto")
+# print_t("tata")
+#
+from chap3_support import add, div
+
+a = 10
+b = 0
+
+#print(a)
+
+#add(2, 3)
+
+#print(a)
+c = div(a, b)
+
+from my_math import factorial
+
+a = factorial(5)
+
