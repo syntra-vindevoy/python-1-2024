af6ea861923cf1e9d58b56a8699c8f2c6af859d7
bertje985 <bert_geeroms@hotmail.com>
2024-10-03 21:46:06 +0200

    adaptions and add exercises from 2nd version

--- chapter03/oef_3_1.py
+++ chapter03/oef_3_1.py
@@ -1,7 +1,28 @@
 def print_right(text, length):
+    text = text.strip() #gooit alle spaties eruit van voor en vanachter
     length_string = len(text)
     leading_spaces = (length - length_string) * " "
 
-    print(f"{leading_spaces}{text}")
+    assert len(text) <= 40, "maximum length of text is 40 characters"
 
-print_right("test", 40)
+    return f"{leading_spaces}{text}"
+
+def main():
+    print(print_right("test", 40))
+    assert len(print_right("test", 40)) == 40
+    assert print_right("test", 40)[36:40] == "test"  #Dit is een test, de string start bij positie 0 tot 39. Dus kan je het laatste ook testen. 36:40 test dus positie 36 tot 39
+    print(print_right("Python", 40))
+    print(print_right("user's always abuse your system  ", 40))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+#asserts bijschrijven
+#de print uit uw functie halen, en gewoon retourneren.
+#vb: assert factorial(0) == 1
--- chapter03/oef_3_2.py
+++ chapter03/oef_3_2.py
@@ -4,4 +4,19 @@ def triangle(letter, height):
         #print(f"{letter}{height},{i}, {leading_spaces}")
         print(leading_spaces + letter * (2* i+1))
 
-triangle("T", 4)
+def triangle_90(letter, height):
+    for i in range(height):
+        print(letter * (i+1))   #hier gaat elke lijn een berekening gebeuren
+
+def triangle_90_betere_oplossing(letter, height):
+    for i in range(1, height +1):  #hier gebeurt maar 1 berekening
+        print(letter * i)
+
+
+def main():
+    triangle("T", 4)
+    triangle_90("T", 4)
+    triangle_90_betere_oplossing("*", 4)
+
+if __name__ == "__main__":
+    main()
