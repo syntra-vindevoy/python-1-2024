cc9c72f77fce85f45b47944a51c3f689c1f043ce
bedoosd <teck_kenneth@hotmail.com>
2024-10-06 11:44:33 +0200

    kerstboom met restricties verbeterd

--- chapter 3/Kerstboom.py
+++ chapter 3/Kerstboom.py
@@ -4,9 +4,11 @@ def kerstboom (char, size):
             print (" " * i + char*(size+2 -i) + char * (size+1-i))
         print (" " * (size+1) + "|")
     else:
+        if len (char) < 1:
+            print ("Enter a character")
         if len (char) > 1:
             print ("Only one character allowed for drawing tree")
         if size < 3:
             print ("Minimum size is 3")
 
-kerstboom ("g", 3)
+kerstboom ("", 5)
