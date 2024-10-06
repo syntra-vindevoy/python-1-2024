fc59960c83afb3b2d74ecb65e4641abcda6fb957
bedoosd <teck_kenneth@hotmail.com>
2024-10-06 09:48:57 +0200

    test

---
+++ chapter 3/grid 2.py
@@ -0,0 +1,14 @@
+def eerste (sta):
+    start = sta
+    return start
+
+def tweede (sto):
+    stop = sto
+    return stop
+
+def totaal (sta, sto):
+    print (eerste(sta))
+    print (tweede(sto))
+
+
+totaal(50,8)
