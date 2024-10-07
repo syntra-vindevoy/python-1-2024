cf860bea3688de1a6083e612681f8c5faa5b315d
GeoffreyCrapoen <crapoen.geoffrey@gmail.com>
2024-10-07 20:54:04 +0200

    Add function to compare two numbers for equality
    Created a new Python script with a function `vergelijken_nummers` to check if two input numbers are equal. The function returns 'true' if they are equal and 'false' otherwise. Also included a test case using hardcoded values for demonstration.

---
+++ test if and else function.py
@@ -0,0 +1,10 @@
+getal_1 = 5
+getal_2 = 4
+
+def vergelijken_nummers (nummer_1, nummer_2):
+    if nummer_1 == nummer_2:
+        return 'true'
+    else:
+        return 'false'
+
+print(vergelijken_nummers(getal_1, getal_2))
