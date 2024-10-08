c4e2f7149537906f9a4b8d7a1bda51fee940b261
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-08 18:53:24 +0200

    Fun test program to create window with turtle

---
+++ Chapter 4/Jupyturtle.py
@@ -0,0 +1,33 @@
+from time import sleep #imports sleep function from time
+from turtle import Turtle #imports Turtle class
+
+# create square window for fun
+
+bob = Turtle()
+
+bob.forward(100)
+
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(200)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(200)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(200)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+
+sleep(10) #freezes frame for 10 seconds
