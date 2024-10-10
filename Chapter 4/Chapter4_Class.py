9d98b3e89a880bfcdc7019b499eb2e063737a978
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-10 21:18:19 +0200

    Added docstrings

--- Chapter 4/Chapter4_Class.py
+++ Chapter 4/Chapter4_Class.py
@@ -1,7 +1,23 @@
 # Les 10/10 oefeningen
 #def fac(n) zonder recursiviteit
 
-def fac(n: int):
+def fac(n: int) -> int: #This HINTS that the value for n should be an integer and also the return value
+    """
+    This function calculates the faculty of a positive integer n. In math this is n!
+
+
+    :param n:
+        int: The number on which we calculate the faculty
+
+    :return:
+        int: The calculated faculty
+
+    Notes:
+        Author: Synix
+        Date: 2024-10-10
+        Version: 1.0.0
+
+    """
     if n < 2:
         return 1
 
