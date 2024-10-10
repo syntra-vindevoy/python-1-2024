fbf45bf7193d716e43f2b02c04947eb4e002e331
Thomas <thomas.meersschaut@gmail.com>
2024-10-10 18:31:16 +0200

    ex_cal created

---
+++ ex_cal.py
@@ -0,0 +1,14 @@
+#Exercise Calendar
+
+def leap_year(*, year: int):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    else
+        return False
+
+def days_month(*, month: int):
+    if (month < 8):
+        if month % 2 == 0
+
+
+
