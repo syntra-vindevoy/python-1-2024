5283a34abbdcacd73b75ac13b10a852d9c9e3f28
YoussefMeziani <meziani.youssef@hotmail.com>
2024-10-10 09:29:05 +0200

    Python 3V chap 04. extra oef verbitteren thuis.

--- Extra oefeningen/param_demo.py
+++ Extra oefeningen/param_demo.py
@@ -1,10 +1,19 @@
-def divide (a, b):
+# def divide (a, b):
+#     return a / b
+#
+# print(divide(6,2))
+# print(divide(2,6))
+#
+# #Onderstaande codes zijn beter.
+# print(divide(a=6,b=2))
+# print(divide(a=2,b=6))
+
+
+def divide(a, b):
     return a / b
 
-print(divide(6,2))
-print(divide(2,6))
+print(divide(6, 2))
+#print(divide(2, 6))
 
-#Onderstaande codes zijn beter.
-print(divide(a=6,b=2))
-print(divide(a=2,b=6))
-
+print(divide(a=6, b=2))
+print(divide(b=2, a=6))
