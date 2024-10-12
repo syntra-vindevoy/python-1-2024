ddc74f2c7ee0ec6d82fb1d8e776d5cf070d35f44
BenoitGoethals <benoit.goethals@gmail.com>
2024-10-12 14:57:57 +0200

    refactor

--- src/chapter3/start.py
+++ src/chapter3/start.py
@@ -2,7 +2,7 @@ import functions_demo
 
 
 def main ():
-    result=functions_demo.calculate_gcd(48,12)
+    result= functions_demo.calculate_gcd(48, 12)
     print(result)
 
 if __name__ == '__main__':
