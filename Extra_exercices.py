2ab99765d472a0ef215401657fee96e9a1b81c22
ralph <ralph.allaert@telenet.be>
2024-10-15 19:11:20 +0200

    Send homework lesson 2

---
+++ Extra_exercices.py
@@ -0,0 +1,17 @@
+def days_of_month(y, m):
+    list31 = (1, 3, 5, 7, 8, 10, 12)
+    list30 = (4, 6, 9, 11)
+
+
+
+
+
+def is_leap(y):
+    return (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+
+is_leap(2024)
+
+
+assert is_leap_year(2024) == true, "test failed"
+
+
