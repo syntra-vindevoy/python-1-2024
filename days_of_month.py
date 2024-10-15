0005ae1fd49e3cd89fa197d32324ffd0c1250e10
Synix1997 <deruyck.sebastien97@gmail.com>
2024-10-15 19:26:12 +0200

    Notes added to dom function + class discussion

--- days_of_month.py
+++ days_of_month.py
@@ -3,6 +3,8 @@
 
 
 #defines leap year and returns True or False
+#Code below can be rewritten to read quicker:
+#return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0): (this returns True or False just like below)
 def is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
@@ -17,7 +19,8 @@ assert is_leap(1) == False
 
 
 #function to return number of days in a month and checking for leap year
-def dom(year, month):
+def dom(year: int, month: int) -> bool:
+    #also easier: if month in [1, 3, 5, 7, 8, 10, 12]:
     if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
         return 31
     elif month == 2:
