570d8dd2c92fc5b87e55208b7e305506f67141e5
ralph <ralph.allaert@telenet.be>
2024-10-15 19:40:07 +0200

    Send homework lesson 2

--- Extra_exercices.py
+++ Extra_exercices.py
@@ -1,17 +1,20 @@
-def days_of_month(y, m):
-    list31 = (1, 3, 5, 7, 8, 10, 12)
-    list30 = (4, 6, 9, 11)
-
-
-
-
-
-def is_leap(y):
-    return (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+def is_leap(year):
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
 is_leap(2024)
 
+def days_of_month(year, month):
+    list31 = (1, 3, 5, 7, 8, 10, 12)
+    list30 = (4, 6, 9, 11)
+    if month in list31:
+        return 31
+    if month in list30:
+        return 30
 
-assert is_leap_year(2024) == true, "test failed"
+
+if __name__ == "__main__":
+    assert is_leap(2024) == True, "test failed"
+
+print(days_of_month(2024, 6))
 
 
