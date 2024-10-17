37f79bb128eb1767179504c3bbe93dee6e68bac5
bedoosd <teck_kenneth@hotmail.com>
2024-10-17 16:26:34 +0200

    added an alternative to draw a pie

--- chapter 4/Turle_exercises/pie_rounded.py
+++ chapter 4/Turle_exercises/pie_rounded.py
@@ -12,6 +12,11 @@ def circle (radius, detail):
     size = (pi * radius * 2) / detail
     polygon (detail, size)
 
+def polyline(n, length, angle):
+    for i in range(n):
+        bob.forward(length)
+        bob.left(angle)
+
 def pie_rounded (size, segments, detail):
     for _ in range (segments):
         bob.forward(size)
@@ -22,5 +27,23 @@ def pie_rounded (size, segments, detail):
     bob.left(90)
     circle(size, detail)
 
-pie_rounded (100,6, 50)
-sleep (5)
+#pie_rounded (100,6, 50)
+#sleep (5)
+
+def arc(radius, angle):
+    #calculates an arc piece and divides it into smaller pieces to it draw using polyline function
+    arc_length = 2 * pi * radius * angle / 360
+    detail = 30
+    length_piece = arc_length / detail
+    step_angle = angle / detail
+    polyline(detail, length_piece, step_angle)
+
+def pie_rounded_alt (size, segments, detail):
+    for _ in range (segments):
+        bob.forward(size)
+        bob.left(90)
+        arc (size , 360 / segments)
+        bob.left(90)
+        bob.forward(size)
+        bob.left(180)
+pie_rounded_alt (100, 5, 30)
