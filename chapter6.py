f12320af0d44378ec4bae5042db083061e151c81
ralph <ralph.allaert@telenet.be>
2024-10-17 20:22:47 +0200

    Send homework lesson 2

--- chapter6.py
+++ chapter6.py
@@ -1,8 +1,7 @@
-from operator import truediv
-
-
-def fac(n/ int) -> int:
+def fac(n: int | n: float) -> int:
     if type(n) != int:
+        if type(n) != float:
+                raise TypeError("fac() only accepts integer or float arguments")
         raise TypeError("fac() only accepts integer arguments")
 
     if n<0:
@@ -15,9 +14,28 @@ assert fac(0) == 1
 assert fac(1) == 1
 
 failed = false
-    try:
-        _= fac(-1)
+try:
+    _= fac(-1)
 except ValueError:
     failed = true
 
 assert failed, "Negative factorial should raise a ValueError"
+
+failed = false
+try:
+    _= fac("a")
+except TypeError:
+    failed = True
+
+assert fac(2.0) ==2
+
+assert failed, "Only integers are valid"
+
+failed = false
+try:
+    _ = fac(2.5)
+except TypeError:
+    failed = True
+
+assert failed, "Only floats are valid"
+
