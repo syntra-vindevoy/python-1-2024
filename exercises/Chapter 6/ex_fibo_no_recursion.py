9f80dfbba85f7aaaf2bb8e2b43e9cafb8e9dd744
chiel <chiel.vansompel@gmail.com>
2024-10-17 21:57:00 +0200

    init file, added example for fibornatci calculation.

---
+++ exercises/Chapter 6/ex_fibo_no_recursion.py
@@ -0,0 +1,37 @@
+def fibo(n: int) -> int:
+    if n < 2:
+        return n
+
+    fibo_0 = 0
+    fibo_1 = 1
+
+    for _ in range(2, n):
+
+        # fibo_0, fibo_1 = fibo_1, fibo_0 + fibo_1
+
+        s = fibo_0 + fibo_1
+        fibo_0 = fibo_1
+        fibo_1 = s
+
+    return fibo_0 + fibo_1
+
+def main():
+
+    print(fibo(5))
+
+    assert fibo(0) == 0
+    assert fibo(1) == 1
+    assert fibo(2) == 1
+    assert fibo(3) == 2
+    assert fibo(4) == 3
+    assert fibo(5) == 5
+    assert fibo(6) == 8
+    assert fibo(7) == 13
+    assert fibo(8) == 21
+
+
+
+
+if __name__ == '__main__':
+    main()
+
