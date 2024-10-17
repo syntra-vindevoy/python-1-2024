51ab313baaff361d5b651585c474db119cdfcff4
Thomas <thomas.meersschaut@gmail.com>
2024-10-17 21:05:47 +0200

    chap06_ex1 & chap06_ex_fibo created

---
+++ chap06_return/chap06_ex_fibo.py
@@ -0,0 +1,28 @@
+#Fibonacci
+
+def fibo(n:int) -> int:
+    if n < 2:
+        return n
+
+    fibo_0 = 0
+    fibo_1 = 1
+
+    for _ in range(2, n):
+        #fibo_0, fibo_1 = fibo_1, (fibo_0 + fibo_1)
+        tmp = fibo_0 + fibo_1
+        fibo_0 = fibo_1
+        fibo_1 = tmp
+
+    return (fibo_0 + fibo_1)
+
+
+
+assert fibo(0) == 0
+assert fibo(1) == 1
+assert fibo(2) == 1
+assert fibo(3) == 2
+assert fibo(4) == 3
+assert fibo(5) == 5
+assert fibo(6) == 8
+assert fibo(7) == 13
+assert fibo(8) == 21
