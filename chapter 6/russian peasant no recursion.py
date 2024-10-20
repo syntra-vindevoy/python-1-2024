0650a8f91c703c2c42a6061b06dbfeb1b3b66472
bedoosd <teck_kenneth@hotmail.com>
2024-10-20 19:47:16 +0200

    added some explanation

--- chapter 6/russian peasant no recursion.py
+++ chapter 6/russian peasant no recursion.py
@@ -1,10 +1,11 @@
 from math import log
 
 def russian_peasant_alt(a,b):
-    if a == 0:
-        return 0
+    if a == 0 or b == 0: return 0 #b == 0 is not necessary for the result but avoids unnecessary calculations
+    if a == 1: return b
+    if b == 1: return a
 
-    check_string (a,b) # deals with false input
+    check_string (a,b) # deals with input of strings
     a, b, negative = check_negative(a,b) #checks and deals with negative numbers
     a, b, factor = if_floats(a,b) #turns floats into integers and calculates the factor to convert the result
     n = int(log(a, 2)) #calculates the number of times a is dividable by 2
@@ -22,7 +23,7 @@ def check_negative (a,b):
     if (a < 0) and (b < 0):
         a *= -1
         b *= -1
-    if (a < 0) ^ (b < 0):
+    elif (a < 0) ^ (b < 0):
         if a < 0:
             a *= -1
         else:
@@ -35,16 +36,19 @@ def check_string (a,b):
         raise SyntaxError ("Values can not be strings")
 
 def if_floats (a,b):
-    dec = 0
-    dec_a = len(str(a)) - len(str(int(a)))
-    dec_b = len(str(b)) - len(str(int(b)))
-    if dec_a != 0:
-        dec += dec_a -1
-        a *= 10 ** (dec_a -1)
-    if dec_b != 0:
-        dec += dec_b -1
-        b *= 10 ** (dec_b -1)
-    factor = 10 ** dec
+    """this function turns floats into integers and calculates the factor
+        to convert the multiplication into a float again.
+        Used in the russian peasant method """
+    decimals = 0
+    decimals_a = len(str(a)) - len(str(int(a))) #calculates the number of decimals
+    decimals_b = len(str(b)) - len(str(int(b)))
+    if decimals_a != 0:
+        decimals += decimals_a -1
+        a *= 10 ** (decimals_a -1)
+    if decimals_b != 0:
+        decimals += decimals_b -1
+        b *= 10 ** (decimals_b -1)
+    factor = 10 ** decimals
     return a, b, factor
 
 
@@ -61,6 +65,7 @@ assert russian_peasant_alt(-5.5, 10) == -55
 assert russian_peasant_alt(1.1, 2) == 2.2
 assert russian_peasant_alt(0.001, 900) == 0.9
 assert russian_peasant_alt(-8.487, 4.87) == -41.33169
+assert russian_peasant_alt(-8.4, 1) == -8.4
 try:
     russian_peasant_alt("a", 5)
 except SyntaxError:
