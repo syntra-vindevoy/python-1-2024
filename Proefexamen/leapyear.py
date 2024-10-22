75ab489f6d8e990df40b092671f5824f25bcb5ca
bedoosd <teck_kenneth@hotmail.com>
2024-10-22 19:00:18 +0200

    leapyear

--- Proefexamen/leapyear.py
+++ Proefexamen/leapyear.py
@@ -1,5 +1,8 @@
 def is_leap_year(year: int) -> bool:
-    return False
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
+    else:
+        return False
 
 
 def main():
