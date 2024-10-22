78a72dde063af2109031da671d9b10771fcc718d
chiel <chiel.vansompel@gmail.com>
2024-10-22 19:24:14 +0200

    added code for returning the number of days for a give month

---
+++ Proefexamen/dom.py
@@ -0,0 +1,54 @@
+# This import is allowed as it's our own function we wrote in leapyear.py
+from leapyear import is_leap_year
+
+
+def days_of_month(year: int, month: int) -> int:
+    # January == 1 !
+    if month == 4 or month == 6 or month == 9 or month == 11:
+            return 30
+
+    elif month == 2:
+
+        if is_leap_year(year):
+            return 29
+
+        return 28
+
+    return 31
+
+
+    pass
+
+
+def main():
+    # Leap year
+    assert days_of_month(2020, 1) == 31  # Jan
+    assert days_of_month(2020, 2) == 29  # Feb
+    assert days_of_month(2020, 3) == 31  # Mar
+    assert days_of_month(2020, 4) == 30  # Apr
+    assert days_of_month(2020, 5) == 31  # May
+    assert days_of_month(2020, 6) == 30  # Jun
+    assert days_of_month(2020, 7) == 31  # Jul
+    assert days_of_month(2020, 8) == 31  # Aug
+    assert days_of_month(2020, 9) == 30  # Sep
+    assert days_of_month(2020, 10) == 31  # Oct
+    assert days_of_month(2020, 11) == 30  # Nov
+    assert days_of_month(2020, 12) == 31  # Dec
+
+    # No leap year
+    assert days_of_month(2022, 1) == 31
+    assert days_of_month(2022, 2) == 28
+    assert days_of_month(2022, 3) == 31
+    assert days_of_month(2022, 4) == 30
+    assert days_of_month(2022, 5) == 31
+    assert days_of_month(2022, 6) == 30
+    assert days_of_month(2022, 7) == 31
+    assert days_of_month(2022, 8) == 31
+    assert days_of_month(2022, 9) == 30
+    assert days_of_month(2022, 10) == 31
+    assert days_of_month(2022, 11) == 30
+    assert days_of_month(2022, 12) == 31
+
+
+if __name__ == "__main__":
+    main()
