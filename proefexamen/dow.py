00b25d4ebbca75a4c796e6c7a92817a1067e78d4
bertje985 <bert_geeroms@hotmail.com>
2024-10-22 20:04:55 +0200

    commit 2.3: add docstrings and comment sunday is 7

--- proefexamen/dow.py
+++ proefexamen/dow.py
@@ -1,8 +1,27 @@
 def day_of_the_week(year: int, month: int, day: int) -> int:
+    """
+            calculate witch day of the week we are.
+
+            it uses the Tomohiko Sakamoto’s Algorithm to find the number of the day of the week.
+
+            Returns:
+                int: the number of the day of the week.
+
+            Examples:
+                day_of_the_week(2022, 12, 1) -> 4
+
+            Version: 1.0.0
+ #          Author: bgeeroms
+ #          Date: 2024-10-22
+    """
     offset = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
     year -= month < 3
     dow = (year + year // 4 - year // 100 + year // 400 + offset[month-1] + day) % 7
 
+    # In europe monday is the first day of the week,
+    # the original function above is from japan, here sunday is the first day of the week
+    # in the function above, when the modulo is 0, its sunday, in Europe day 7, so wo rematch 0 to 7 for sunday
+    # the other modulo is correct. 1 for monday, 2 for tuesday
     if dow == 0:
         dow = 7
 
