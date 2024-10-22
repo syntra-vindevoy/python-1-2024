9cb743e5e9a8e5040aa64fbbb586a4841d30a1da
mvdholen <marijnvandenholen@gmail.com>
2024-10-22 21:19:28 +0200

    opdracht 2.5 getest op 3 maanden

--- proefexamen/calendar.py
+++ proefexamen/calendar.py
@@ -6,9 +6,9 @@ from proefexamen.dom import days_of_month
 
 def print_calendar(year: int, mont: int) -> None:
     first_day_of_week = dow.day_of_the_week(year, mont, 1)
-    print(f"eerste dag van de maand: dag {first_day_of_week} {ds.name_of_day(first_day_of_week)}")
+    # print(f"eerste dag van de maand: dag {first_day_of_week} {ds.name_of_day(first_day_of_week)}")
 
-    print(f'{ds.name_of_month(12)} {year}')
+    print(f'\n{ds.name_of_month(mont)} {year}')
     print()
     for i in range(1, 8):
         print(ds.name_of_day(i), end=' ')
@@ -25,14 +25,18 @@ def print_calendar(year: int, mont: int) -> None:
 
         print()
 
-    for i in range(days_of_month(year, mont) // 7 + 1):
-        print_one_line(first_day_of_week - 6, first_day_of_week + 1)
-        first_day_of_week += 7
+    weeks_in_month = 6
+    start = 1 - first_day_of_week + 1
+    for i in range(weeks_in_month):
+        # print_one_line(0, 7)
+        print_one_line(start, start + 7)
+        start += 7
 
 
 def main():
+    print_calendar(2022, 10)
+    print_calendar(2022, 11)
     print_calendar(2022, 12)
-    # print_calendar(2022, 12)
 
 
 if __name__ == "__main__":
