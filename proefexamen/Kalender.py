90157236554d03be36f65ab89641f4e3feb6355c
Alvaro Avelar <alvaro1alexander@gmail.com>
2024-10-22 21:18:34 +0200

    Dit is een tussendoorse commit van proefexamen

--- proefexamen/Kalender.py
+++ proefexamen/Kalender.py
@@ -5,9 +5,10 @@ from dom import *
 
 def kalender(year: int, month: int, day: int) -> int:
     name_of_month(month)
-    print(name_of_month(month), year,)
+    print(name_of_month(month), year)
     print()
-
+    dow = day_of_the_week(year, month, day)
+    print(name_of_day(dow))
 
 
 print(kalender(2020, 1, 1))
