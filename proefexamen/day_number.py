cb9cb2f2846a5cc140f94f8b3543dd12a45a0730
bertje985 <bert_geeroms@hotmail.com>
2024-10-22 20:48:46 +0200

    commit 2.5: calculate nbr day of year

--- proefexamen/day_number.py
+++ proefexamen/day_number.py
@@ -2,12 +2,25 @@ from dom import *  # importing our days of month
 
 
 def day_number(year: int, month: int, day: int) -> int:
-    pass
+    x = 0
+
+    if month < 3:
+        if month == 1:
+            return x + day
+        return x + 31 + day
+    else:
+        for m in range(1, month):
+            x += days_of_month(year, m)
+        x = x + day
+
+    return x
 
 
 def main():
     assert day_number(2020, 1, 1) == 1
+    assert day_number(2020, 1, 15) == 15
     assert day_number(2020, 2, 1) == 32
+    assert day_number(2020, 2, 15) == 46
     assert day_number(2020, 3, 1) == 61
     assert day_number(2020, 12, 31) == 366
 
