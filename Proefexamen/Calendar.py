d8f8ac5e782a902e05574bc3db5d0eb0ed9b7d3a
bedoosd <teck_kenneth@hotmail.com>
2024-10-22 21:15:35 +0200

    5 lines but not correct

--- Proefexamen/Calendar.py
+++ Proefexamen/Calendar.py
@@ -12,12 +12,28 @@ def print_calendar(year=int, month=int):
     print (line_1)
 
     line_2 = ("")
-    for i in range(1+7, 9+7 - dow):
+    for i in range(9 - dow, 9-dow+7):
         line_2 += str(i) + "  "
     print (line_2)
 
-print_calendar(2024, 10)
+    line_3 = ("")
+    for i in range(9-dow + 7 , 9-dow + 14):
+        line_3 += str(i) + "  "
+    print (line_3)
+
+    line_4 = ("")
+    for i in range(9-dow + 14 , 9-dow + 21):
+        line_4 += str(i) + "  "
+    print (line_4)
+
+    line_5 = ("")
+    for i in range(9-dow + 21, days_of_month(year = year, month = month)+1):
+        line_5 += str(i) + "  "
+    print (line_5)
 
 
 
 
+
+
+print_calendar(2024, 12)
