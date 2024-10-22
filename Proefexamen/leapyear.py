98723c0d4c7e9927fd3b7d78dc1525cd362baf24
bedoosd <teck_kenneth@hotmail.com>
2024-10-22 18:28:32 +0200

    created proefexamen

---
+++ Proefexamen/leapyear.py
@@ -0,0 +1,20 @@
+def is_leap_year(year: int) -> bool:
+    return False
+
+
+def main():
+    # Not dividable by 4
+    assert is_leap_year(2021) is False
+
+    # Dividable by 4, not by 100
+    assert is_leap_year(2020) is True
+
+    # Dividable by 100, not by 400
+    assert is_leap_year(2100) is False
+
+    # Dividable by 100 and 400
+    assert is_leap_year(2000) is True
+
+
+if __name__ == "__main__":
+    main()
