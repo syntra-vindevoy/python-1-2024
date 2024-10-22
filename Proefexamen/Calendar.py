5579e3bb8e6f2e16ffa3bdea02a8d5bce6a2d776
bedoosd <teck_kenneth@hotmail.com>
2024-10-22 21:54:38 +0200

    working calendar

--- Proefexamen/Calendar.py
+++ Proefexamen/Calendar.py
@@ -1,39 +1,18 @@
 from Proefexamen.dom import days_of_month
 from dow import day_of_the_week
+from date_string import name_of_month
 
 def print_calendar(year=int, month=int):
-    print (f"{month}  {year}\n\n")
+    print (f"{name_of_month(month)}  {year}\n\n")
     print (f"Ma Di Wo Do Vr Za Zo")
     dow = day_of_the_week(year = year, month = month, day = 1)
+    dom = days_of_month(year = year, month = month)
 
-    line_1 = ("  " * dow)
-    for i in range(1, 9 - dow):
-        line_1 += str(i) + "  "
-    print (line_1)
+    line = (dow - 1) * "   "
+    for i in range (1, dom + 1):
+        line += str(f"{i:2} ")  #aan chat gpt gevraagd hoe ik de dagen kon uitlijnen
+        if (i-1 + dow) % 7 == 0:
+            line += "\n"
+    print (line)
 
-    line_2 = ("")
-    for i in range(9 - dow, 9-dow+7):
-        line_2 += str(i) + "  "
-    print (line_2)
-
-    line_3 = ("")
-    for i in range(9-dow + 7 , 9-dow + 14):
-        line_3 += str(i) + "  "
-    print (line_3)
-
-    line_4 = ("")
-    for i in range(9-dow + 14 , 9-dow + 21):
-        line_4 += str(i) + "  "
-    print (line_4)
-
-    line_5 = ("")
-    for i in range(9-dow + 21, days_of_month(year = year, month = month)+1):
-        line_5 += str(i) + "  "
-    print (line_5)
-
-
-
-
-
-
-print_calendar(2024, 12)
+print_calendar(2024, 7)
