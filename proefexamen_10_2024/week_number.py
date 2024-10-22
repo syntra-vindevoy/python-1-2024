98261ddc2efd16485bbeb557ba8a1b6e0d9777a9
Thomas <thomas.meersschaut@gmail.com>
2024-10-22 21:05:08 +0200

    Opdracht 2.8

--- proefexamen_10_2024/week_number.py
+++ proefexamen_10_2024/week_number.py
@@ -1,9 +1,17 @@
+from dow import *
+from day_number import *
+
 def week_number(year: int, month: int, day: int):
-    pass
+    dow = day_of_the_week(year, 1, 1)
+    days = day_number(year, month, day)
+    week = (dow - 2 + days) // 7
+    return week + 1
 
 
 def main():
     assert week_number(2022, 1, 1) == 1
+    assert week_number(2022, 2, 28) == 10
+    assert week_number(2022, 10, 22) == 43
 
 
 if __name__ == "__main__":
