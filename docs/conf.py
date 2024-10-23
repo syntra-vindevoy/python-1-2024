6ca7f5575f56fbbc30fccd5d70595931bda5c064
BenoitGoethals <benoit.goethals@gmail.com>
2024-10-23 17:56:52 +0200

    Remove outdated documentation files
    Deleted multiple .rst documentation files under the docs and src/docs directories due to outdated content. These files were no longer relevant to the current state of the codebase and posed potential confusion for developers.

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..\src'))
+sys.path.insert (0, os.path.abspath ('../src'))
 
 project = 'project2024-1-benoit'
 copyright = '2024, benoit'
