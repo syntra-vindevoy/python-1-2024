43240c4cc13358b55e6feb270ff321acd7a1bd19
chiel <chiel.vansompel@gmail.com>
2024-10-25 19:18:54 +0200

    Finished exercise 2 from book 2

--- exercises/Chapter 7/mysqr.py
+++ exercises/Chapter 7/mysqr.py
@@ -1,22 +1,40 @@
 import math
 
-def mysqrt(a, x):
+def mysqrt(a: int):
+
+    x = a - 1 if a != 1 else 1
+
     while True:
-        print(x)
         y = (x + a / x) / 2
         if y == x:
             break
 
         x = y
 
-def mathsqrt(a):
+    return x
+
+def mathsqrt(a: int):
     return math.sqrt(a)
 
-def test_square_root(a):
+def test_square_root(amount: int):
+
+    start_nr = 1
+    sqrt_nr = 1.0
+
+    print("a", " " * 2, "mysqrt(a)", " " * 3, "mathsqrt(a)", " " * 1,  "diff")
+    print("-", " " * 2, "-" * 9, " " * 3, "-" * 11, " ", "-" * 4)
+    while amount >= start_nr:
+
+        value1 = mysqrt(a = start_nr)
+        value2 = mathsqrt(a = start_nr)
+        value3 = abs(value1 - value2)
+        print(f"{sqrt_nr}  {value1:.10f}  {value2:.10f}  {value3:.10e}")
+        start_nr += 1
+        sqrt_nr += 1
 
 
 def main():
-    mysqrt(2,1)
+    test_square_root(9)
 
 if __name__ == "__main__":
     main()
