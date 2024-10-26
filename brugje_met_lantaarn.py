bb46dbf3d710775b75d6404f8adf4802e30992d0
mvdholen <marijnvandenholen@gmail.com>
2024-10-26 23:32:24 +0200

    end of day

--- brugje_met_lantaarn.py
+++ brugje_met_lantaarn.py
@@ -1,17 +1,11 @@
 def generate_all_combinations():
-    # mistake in logic, one person can return more than one time...
+    # mistake in logic, one person can return more than one time, potential solutions missing...
     base = [1, 2, 5, 10]
     solutions = []
-    time_spent = 0
     for person1 in base:
         for person2 in base:
-            time_spent += max(person1, person2)
-            time_spent += person2
             for person3 in base:
-                time_spent += max(person2, person3)
-                time_spent += person3
                 for person4 in base:
-                    time_spent += max(person3, person4)
                     if (
                         person1 != person2 != person3 != person4
                         and person1 != person3
@@ -19,7 +13,6 @@ def generate_all_combinations():
                         and person1 != person4
                     ):
                         solutions.append((person1, person2, person3, person4))
-                    time_spent = 0
     return solutions
 
 
@@ -33,12 +26,7 @@ def calculate_time_spent(person1, person2, person3, person4):
     return time_spent
 
 
-def tests():
-    pass
-
-
 def main():
-    tests()
     list_combinations = generate_all_combinations()
     list_times_per_combination = []
 
