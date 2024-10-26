56a4b7235df2df7aa8df80c1c26992e5a0f48577
mvdholen <marijnvandenholen@gmail.com>
2024-10-26 20:40:51 +0200

    finished square_root.py

--- square_root.py
+++ square_root.py
@@ -1,3 +1,6 @@
+import math
+
+
 def square_root(number, estimate):
     while True:
         result = (estimate + number / estimate) / 2
@@ -5,7 +8,27 @@ def square_root(number, estimate):
             return estimate
             break
         estimate = result
-        print(f"estimate: {estimate}")
+        # print(f"estimate: {estimate}")
+
+
+def print_table():
+    def print_header():
+        column1 = "a"
+        column2 = f"square_root(a,1)"
+        column3 = "math.sqrt(a)"
+        column4 = "diff"
+        print(f"{column1:10} {column2:20} {column3:20} {column4:22}")
+
+    def print_body():
+        for i in range(1, 11):
+            column1 = i
+            column2 = square_root(i, 1)
+            column3 = math.sqrt(i)
+            column4 = abs(column2 - column3)
+            print(f"{column1:10} {column2:20} {column3:20} {column4:22}")
+
+    print_header()
+    print_body()
 
 
 def tests():
@@ -15,9 +38,7 @@ def tests():
 
 def main():
     tests()
-    print(square_root(9, 3))
-    print(square_root(25, 4))
-    print(square_root(100, 25))
+    print_table()
 
 
 if __name__ == "__main__":
