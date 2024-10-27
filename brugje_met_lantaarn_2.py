b7ac97aa8a8fda0a394892e39274bc370d00d198
mvdholen <marijnvandenholen@gmail.com>
2024-10-27 13:51:50 +0100

    brug.py: trying to work with situations

--- brugje_met_lantaarn_2.py
+++ brugje_met_lantaarn_2.py
@@ -1,41 +1,63 @@
-start_situation = [[1, 2, 5, 10], [], 0]
+situation_left = [1, 2, 5, 10]
+situation_right = []
 
-
-def move_to_right(num: int):
-    if num in start_situation[0]:
-        start_situation[0].remove(num)
-        start_situation[1].append(num)
-        return True
-    return False
+situation = [situation_left, situation_right, 0]
+situations = [situation]
 
 
 def move_right(
-    person: int, start_situation: list[list[int], list[int], int]
-) -> list[list[int], list[int], int]:
-    new_situation = start_situation.copy()
-    new_situation[0].remove(person)
-    new_situation[1].append(person)
-    return new_situation
+    personA: int,
+    personB: int,
+    start_situation: list[list[int], list[int], int],
+) -> tuple[list[int], list[int], int]:
+
+    new_situation_left = start_situation[0].copy()
+    new_situation_left.remove(personA)
+    new_situation_left.remove(personB)
+
+    new_situation_right = start_situation[1].copy()
+    new_situation_right.append(personA)
+    new_situation_right.append(personB)
+
+    new_time_spent = start_situation[2]
+    new_time_spent += max(personA, personB)
+
+    return [new_situation_left, new_situation_right, new_time_spent]
 
 
 def move_left(
-    person: int, start_situation: list[list[int], list[int], int]
-) -> list[list[int], list[int], int]:
-    new_situation = start_situation.copy()
-    new_situation[1].remove(person)
-    new_situation[0].append(person)
-    return new_situation
+    person: int,
+    start_situation: list[list[int], list[int], int],
+) -> tuple[list[int], list[int], int]:
+
+    new_situation_left = start_situation[0].copy()
+    new_situation_left.append(person)
+
+    new_situation_right = start_situation[1].copy()
+    new_situation_right.remove(person)
+
+    new_time_spent = start_situation[2]
+    new_time_spent += person
+
+    return [new_situation_left, new_situation_right, new_time_spent]
 
 
 def main():
-    print(start_situation)
-    new_situation = start_situation.copy()
-    new_situation = move_right(1, new_situation)
-    print(new_situation)
-    new_situation = move_left(1, new_situation)
-    print(new_situation)
-    """
-    """
+    situation_left = [1, 2, 5, 10]
+    situation_right = []
+
+    situation = [situation_left, situation_right, 0]
+    situations = [situation]
+
+    while True:
+        for situation in situations:
+            generate_new_situations_moving_to_right(situation, situations)
+
+
+def tests():
+    assert move_right(1, 2, [[1, 2, 5, 10], [], 0]) == [[5, 10], [1, 2], 2]
+    assert move_left(1, [[5, 10], [1, 2], 2]) == [[5, 10, 1], [2], 3]
+    pass
 
 
 if __name__ == "__main__":
