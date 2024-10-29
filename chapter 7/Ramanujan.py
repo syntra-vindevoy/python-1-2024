36e24006f7b2e63513a29eec88d47a7fe15f70a1
bedoosd <teck_kenneth@hotmail.com>
2024-10-29 16:45:38 +0100

    created function

--- chapter 7/Ramanujan.py
+++ chapter 7/Ramanujan.py
@@ -1,16 +1,17 @@
 
 import math
-
-formula_fix = (2 * math.sqrt(2)) / 9801
-result = 0
-k = 0
-formula_var = (math.factorial(4 * k) * (1103 + 26390 * k)) / ((math.factorial(k) ** 4) * (396 ** (4 * k)))
-
-while (formula_fix * formula_var) > 1e-15:
+def estimate_pi ():
+    formula_fix = (2 * math.sqrt(2)) / 9801
+    result = 0
+    k = 0
     formula_var = (math.factorial(4 * k) * (1103 + 26390 * k)) / ((math.factorial(k) ** 4) * (396 ** (4 * k)))
-    result += formula_fix * formula_var
-    k += 1
+
+    while (formula_fix * formula_var) > 1e-15:
+        formula_var = (math.factorial(4 * k) * (1103 + 26390 * k)) / ((math.factorial(k) ** 4) * (396 ** (4 * k)))
+        result += formula_fix * formula_var
+        k += 1
+    return result
 
 if __name__ == "__main__":
-    print ( f"Approximated Pi = {1 / result}")
+    print ( f"Approximated Pi = {1 / estimate_pi()}")
     print ( f"Pi from math.pi = {math.pi}")
