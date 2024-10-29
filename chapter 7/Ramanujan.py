1a1b07de01bf575533abe86e4df1b5fd116d911a
bedoosd <teck_kenneth@hotmail.com>
2024-10-29 16:24:47 +0100

    completed

--- chapter 7/Ramanujan.py
+++ chapter 7/Ramanujan.py
@@ -1,14 +1,16 @@
-"""Exercise 7.3. The mathematician Srinivasa Ramanujan found an infinite series that can be used to
-generate a numerical approximation of 1/π:
-Write a function called estimate_pi that uses this formula to compute and return an estimate of
-π. It should use a while loop to compute terms of the summation until the last term is smaller than
-1e-15 (which is Python notation for 10−15). You can check the result by comparing it to math.pi."""
+
 import math
 
 formula_fix = (2 * math.sqrt(2)) / 9801
 result = 0
-for k in range (0, 2000):
+k = 0
+formula_var = (math.factorial(4 * k) * (1103 + 26390 * k)) / ((math.factorial(k) ** 4) * (396 ** (4 * k)))
+
+while (formula_fix * formula_var) > 1e-15:
     formula_var = (math.factorial(4 * k) * (1103 + 26390 * k)) / ((math.factorial(k) ** 4) * (396 ** (4 * k)))
     result += formula_fix * formula_var
+    k += 1
 
-print (1 / result)
+if __name__ == "__main__":
+    print ( f"Approximated Pi = {1 / result}")
+    print ( f"Pi from math.pi = {math.pi}")
