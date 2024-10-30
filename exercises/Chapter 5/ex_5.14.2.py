a1792a35a1a98d79d4da7aa292810a6a13e85812
chiel <chiel.vansompel@gmail.com>
2024-10-30 14:16:54 +0100

    added solution without using tuples

--- exercises/Chapter 5/ex_5.14.2.py
+++ exercises/Chapter 5/ex_5.14.2.py
@@ -34,10 +34,45 @@ def time_calc(epoch_time: float) -> tuple[float, float, float, float]:
 
     return days, hours, minutes, seconds
 
+def time_calc2(epoch_time: float) -> str:
+
+    """
+    Function to calculate how many days, hours, minutes, and seconds have passed since the start of epoch time.
+
+    Parameters
+    ----------
+    epoch_time : float
+        The amount of seconds since the start of epoch time.
+
+    Returns
+    -------
+    str
+        compiling the answer needed and returning it as a string.
+
+    Author
+    ------
+    Chiv
+
+    Date
+    ----
+    30-10-2024
+    """
+
+    days = int(epoch_time // (24 * 3600))
+    hours = int(epoch_time // 3600 % 24)
+    minutes = int(epoch_time // 60 % 60)
+    seconds = int(epoch_time % 60)
+
+    answer = f"Tijd: {days} dagen, {hours} uur, {minutes} minuten en {seconds} seconden"
+
+    return answer
+
 def main():
     days, hours, minutes, seconds = time_calc(epoch_time)
     print(f"Tijd: {int(days)} dagen, {int(hours)} uur, "
           f"{int(minutes)} minuten en {round(seconds)} seconden")
+
+    print(time_calc2(epoch_time))
 
 if __name__ == "__main__":
     main()
