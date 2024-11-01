00e6948f2772533d739231de2736369a33c9b496
BenoitGoethals <benoit.goethals@gmail.com>
2024-11-01 17:07:43 +0100

    Add Ramanujan's pi estimation method
    Introduced an implementation of Ramanujan's series for pi approximation. Also refactored existing code and added comments to better organize and explain the exercises.

--- src/chapter7/ex_7_book2.py
+++ src/chapter7/ex_7_book2.py
@@ -1,6 +1,8 @@
 import math
 
-
+"""
+Exercise 1  
+"""
 def my_sqrt(a: float):
     x=3
     while True:
@@ -14,10 +16,35 @@ for i in range(1,10):
     m:float=math.sqrt(i)
     print (f"{i} {mx:.8f}    {m:.8f}     {mx - m:.8f}")
 
+"""
+Exercise 3 
+The mathematician Srinivasa Ramanujan found an infinite series that can be used to generate a numerical
+ approximation of 1 / π:
+"""
 
-# Cardtalk puzzelers money machine
-# is primegetal
-# sum van 1 tot 10
-# 1  11  21  1211 111221 312211
+
+def factorial (n):
+    if n == 0:
+        return 1
+    else:
+        result = n * factorial (n - 1)
+        return result
 
 
+def estimate_pi ():
+    total = 0
+    k = 0
+    factor = 2 * math.sqrt (2) / 9801
+    while True:
+        num = factorial (4 * k) * (1103 + (26390 * k))
+        divider = factorial (k) ** 4 * 396 ** (4 * k)
+        total += num / divider
+        term = factor * num / divider
+        if abs (term) < 1e-15:
+            break
+        k += 1
+
+    return 1 / (factor * total)
+
+
+print (estimate_pi ())
