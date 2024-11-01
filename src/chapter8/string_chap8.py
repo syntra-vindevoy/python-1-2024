e53a6c55adb736e4e7573a679f159d76b27fc66b
benoitgoethals <benoit.goethals@gmail.com>
2024-11-01 20:42:58 +0100

    Add is_reverse function to string_chap8.py
    This function checks if one string is the reverse of another by comparing characters from the start of the first string to the end of the second string. It returns False if the strings have different lengths or if any characters do not match. Additionally, an assertion is included to test the function.

---
+++ src/chapter8/string_chap8.py
@@ -0,0 +1,13 @@
+def is_reverse(word1, word2):
+    if len(word1) != len(word2):
+        return False
+    i = 0
+    j = len(word2)-1
+    while j > 0:
+        if word1[i] != word2[j]:
+            return False
+        i += 1
+        j -=1
+    return True
+
+assert is_reverse( "sprint", "tnirps")==True
