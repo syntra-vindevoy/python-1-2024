a1855d2b5289abad7adc078c047996f7e294fae5
bedoosd <teck_kenneth@hotmail.com>
2024-11-07 17:49:28 +0100

    cleaned op the wom code and calendar

--- Proefexamen/wom.py
+++ Proefexamen/wom.py
@@ -2,22 +2,20 @@ from Proefexamen.dom import days_of_month
 from dow import day_of_the_week
 
 def next_month_start_weeks (year, month):
-    #calculates the number of unfull weeks, if month stops on a sunday, the next month starts in a new week
-    if days_of_month == 29 and day_of_the_week (year, month, 1) == 7:
+    #calculates the number of weeks till next month, if month stops on a sunday, the next month starts in a new week
+    if days_of_month == 29 and day_of_the_week (year, month, 1) == 7: #only exception for february
         return 6
-    if month in (1,3,5,7,8,10,12):
-        if day_of_the_week(year, month, 1) > 4:
-            return 6
-    if month in (4,6,9,11):
-        if day_of_the_week(year, month, 1) > 5:
-            return 6
+    if month in (1,3,5,7,8,10,12) and day_of_the_week(year, month, 1) > 4:
+        return 6
+    if month in (4,6,9,11) and day_of_the_week(year, month, 1) > 5:
+        return 6
     return 5
+
 def month_start_week (year, month):
-    x = 1
-    if month == 1: return 1
+    total_weeks = 1
     for i in range(1, month):
-        x += next_month_start_weeks(year, i) - 1#to not double count the first week of the month
-    return x
+        total_weeks += next_month_start_weeks(year, i) - 1#to not double count the first week of the month
+    return total_weeks
 
 assert month_start_week(2018, 1) == 1
 assert month_start_week(2016, 5) == 18
