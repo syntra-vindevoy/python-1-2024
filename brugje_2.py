b6121c9a92150a70de1eaeab996c620af1f9eb40
mvdholen <marijnvandenholen@gmail.com>
2024-11-07 23:13:23 +0100

    still trying brugje

--- brugje_2.py
+++ brugje_2.py
@@ -1,68 +1,19 @@
-situation_left = [1, 2, 5, 10]
-situation_right = []
+from itertools import combinations
 
-situation = [situation_left, situation_right, 0]
+
+situation = {"left": [1, 2, 5, 10], "right": [], "time_spent": 0}
 situations = [situation]
 
 
-def move_right(
-    personA: int,
-    personB: int,
-    start_situation: list[list[int], list[int], int],
-) -> tuple[list[int], list[int], int]:
-
-    new_situation_left = start_situation[0].copy()
-    new_situation_left.remove(personA)
-    new_situation_left.remove(personB)
-
-    new_situation_right = start_situation[1].copy()
-    new_situation_right.append(personA)
-    new_situation_right.append(personB)
-
-    new_time_spent = start_situation[2]
-    new_time_spent += max(personA, personB)
-
-    return [new_situation_left, new_situation_right, new_time_spent]
+comb = combinations(situation["left"], 2)
 
 
-assert move_right(1, 2, [[1, 2, 5, 10], [], 0]) == [[5, 10], [1, 2], 2]
+for situation in situations:
+    for c in comb:
 
-
-def move_left(
-    person: int,
-    start_situation: list[list[int], list[int], int],
-) -> tuple[list[int], list[int], int]:
-
-    new_situation_left = start_situation[0].copy()
-    new_situation_left.append(person)
-
-    new_situation_right = start_situation[1].copy()
-    new_situation_right.remove(person)
-
-    new_time_spent = start_situation[2]
-    new_time_spent += person
-
-    return [new_situation_left, new_situation_right, new_time_spent]
-
-
-assert move_left(1, [[5, 10], [1, 2], 2]) == [[5, 10, 1], [2], 3]
-
-
-def remove_situation(situation, situations):
-    situations.remove(situation)
-
-
-def main():
-    situation_left = [1, 2, 5, 10]
-    situation_right = []
-
-    situation = [situation_left, situation_right, 0]
-    situations = [situation]
-
-
-def tests():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+        new_situation = situation.copy()
+        new_situation["left"] = [x for x in new_situation["left"] if x not in c]
+        new_situation["right"] = list(c)
+        new_situation["time_spent"] += max(c)
+        situations.append(new_situation)
+    print(situation)
