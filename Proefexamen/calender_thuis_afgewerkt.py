24fab7c5cbdb5d842723a1066450039045b40875
bedoosd <teck_kenneth@hotmail.com>
2024-11-07 21:58:00 +0100

    Adjusted to start at week 52 of end at week 1 if necessary

--- Proefexamen/calender_thuis_afgewerkt.py
+++ Proefexamen/calender_thuis_afgewerkt.py
@@ -10,15 +10,20 @@ def print_calendar(year=int, month=int):
     dom = days_of_month(year = year, month = month)
 
     week = month_start_week(year = year, month = month)
-    line = f"[{week}]".ljust(5) + (dow - 1) * "   "
+    if week == 0:
+        line = f"[{52}]".ljust(5) + (dow - 1) * "   "
+    else:
+        line = f"[{week}]".ljust(5) + (dow - 1) * "   "
 
     for i in range (1, dom + 1):
         line += f"{i:2} "
         if ((i-1 + dow) % 7 == 0) and i != dom: #anders komt dit niet mooi uit bij jaarkalender
             week += 1
+            if week == 53 and day_of_the_week(year,12,31) <= 3 : week = 1
+            #the last week of the year has to contain december 28, else it is the first week of the next year
             line += "\n" + f"[{week}] ".ljust(5)
 
-        print (line)
+    print (line)
 
 
 def print_year_calendar(year):
@@ -26,4 +31,4 @@ def print_year_calendar(year):
         print_calendar(year,i)
         print ("")
 
-print_year_calendar(2002)
+print_year_calendar(2024)
