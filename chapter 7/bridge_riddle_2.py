fa8d451480efe54a217ec8df5bd069351e29475f
bedoosd <teck_kenneth@hotmail.com>
2024-11-08 21:47:05 +0100

    bridge riddle restart

--- chapter 7/bridge_riddle_2.py
+++ chapter 7/bridge_riddle_2.py
@@ -17,8 +17,21 @@ def count_time_pair (pair):
     return time
 
 def move_right_to_left (left, right, single):
-    left.extend (single)
+    left.append (single)
     right.remove (single)
     return left, right
+
+def bridge_riddle(left,right):
+    pairs = split_in_pairs (left)
+    time = 0
+    for i in range(len(pairs)):
+        left, right = move_left_to_right (left,right,pairs[i])
+        time += count_time_pair(pairs[i])
+        for j in range (len(right)):
+            left, right = move_right_to_left(left,right,j)
+            time += right [j]
+    return time
+
+print (bridge_riddle([1,5,6,8],[]))
 
 
