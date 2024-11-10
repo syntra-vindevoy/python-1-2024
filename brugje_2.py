6b4e4bff9899fdaadae565f8d9e656bb904df755
mvdholen <marijnvandenholen@gmail.com>
2024-11-10 21:33:00 +0100

    moved two people to right, one to left succeeded

--- brugje_2.py
+++ brugje_2.py
@@ -1,19 +1,47 @@
 from itertools import combinations
 
+START_SITUATION = {
+    "left_people": [1, 2, 5, 10],
+    "right_people": [],
+    "time_spent": 0,
+}
 
-situation = {"left": [1, 2, 5, 10], "right": [], "time_spent": 0}
-situations = [situation]
+
+def get_move_to_right_combinations(situation):
+    return list(combinations(situation["left_people"], 2))
 
 
-comb = combinations(situation["left"], 2)
+def move_two_persons_to_right(situation, person1, person2):
+    situation["left_people"].remove(person1)
+    situation["left_people"].remove(person2)
+    situation["right_people"].append(person1)
+    situation["right_people"].append(person2)
+    situation["time_spent"] += max(person1, person2)
+    return situation
 
 
-for situation in situations:
-    for c in comb:
+def get_move_to_left_combinations(situation):
+    return situation["right_people"]
 
-        new_situation = situation.copy()
-        new_situation["left"] = [x for x in new_situation["left"] if x not in c]
-        new_situation["right"] = list(c)
-        new_situation["time_spent"] += max(c)
-        situations.append(new_situation)
-    print(situation)
+
+def move_one_person_to_left(situation, person):
+    situation["right_people"].remove(person)
+    situation["left_people"].append(person)
+    situation["time_spent"] += person
+    return situation
+
+
+def main():
+    situation = START_SITUATION.copy()
+    situations = [situation]
+    for situation in situations:
+        situations.append(move_two_persons_to_right(situation.copy(), 1, 2))
+        situations.remove(situation)
+    for situation in situations:
+        situations.append(move_one_person_to_left(situation.copy(), 1))
+        situations.remove(situation)
+    print(situations)
+
+
+if __name__ == "__main__":
+    main()
