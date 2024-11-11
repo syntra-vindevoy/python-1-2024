24268f611f2258c4a74bee1db82f4ff8674b91b0
mvdholen <marijnvandenholen@gmail.com>
2024-11-11 19:23:45 +0100

    brugje3, alle situaties na één move naar links ok

--- brugje_3.py
+++ brugje_3.py
@@ -13,21 +13,53 @@ def move_to_right(situation, person1, person2):
     return new_situation
 
 
+def move_to_left(situation, person):
+    new_situation = copy.deepcopy(situation)
+    new_situation["right"].remove(person)
+    new_situation["left"].append(person)
+    new_situation["time_spent"] += person
+    return new_situation
+
+
 def get_combinations_move_to_right(situation):
     return combinations(situation["left"], 2)
 
 
-def main():
-    situation_start = {"left": [1, 2, 5, 10], "right": [], "time_spent": 0}
-    situations = [situation_start]
+def get_combinations_move_to_left(situation):
+    return combinations(situation["right"], 1)
+
+
+def print_all_situations(situations):
+    print()
+    for s in situations:
+        pprint(s)
+
+
+def get_all_situations_moving_to_right(situations):
     situations_new = []
     for s in situations:
         for c in get_combinations_move_to_right(s):
             new_situation = move_to_right(s, c[0], c[1])
             situations_new.append(new_situation)
-    situations = situations_new
+    return situations_new
+
+
+def get_all_situations_moving_to_left(situations):
+    situations_new = []
     for s in situations:
-        pprint(s)
+        for c in get_combinations_move_to_left(s):
+            new_situation = move_to_left(s, c[0])
+            situations_new.append(new_situation)
+    return situations_new
+
+
+def main():
+    situation_start = {"left": [1, 2, 5, 10], "right": [], "time_spent": 0}
+    situations = [situation_start]
+    situations = get_all_situations_moving_to_right(situations)
+    print_all_situations(situations)
+    situations = get_all_situations_moving_to_left(situations)
+    print_all_situations(situations)
 
 
 if __name__ == "__main__":
