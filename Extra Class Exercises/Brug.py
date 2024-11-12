c46487b02cd5bf48bb1733873106fe2096a84f36
Synix1997 <deruyck.sebastien97@gmail.com>
2024-11-12 19:08:29 +0100

    Brug - exercise at home, first commit

---
+++ Extra Class Exercises/Brug.py
@@ -0,0 +1,3 @@
+# Steek de brug over met verschillende mensen, 2 per X en steeds 1 iemand moet terugkeren.
+# 1, 2, 5, 10 min per persoon
+# (oplossing: 1 + 2 en 1 keert terug / 5 + 10 en 2 keert terug / 1 + 2 keren -- 17 min)
