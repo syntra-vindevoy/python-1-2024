1152cfedd99634b25fa0622eb83b05748a604cd7
benoitgoethals <benoit.goethals@gmail.com>
2024-11-12 19:53:12 +0100

    #10 Add Line and Rectangle methods for various geometrical computations
    Implemented equality comparison for the Line class and added methods for calculating midpoints in Line and Rectangle classes. Also included functions to create lines for rectangle sides and generate cross lines connecting rectangle midpoints.

---
+++ src/chapter7/romain.py
@@ -0,0 +1,19 @@
+"""
+ = I
+2 = II
+3 = III
+4 = IV
+5 = V
+6 = VI
+7 = VII
+8 = VIII
+9 = IX
+10 = X
+20 = XX
+50 = L
+100 = C
+500 = D
+1000 = M
+
+https://www.mijnrekensite.nl/romeinse-cijfers
+"""
