fae59689d3133a82b18b7f0be8266b79312e3730
bedoosd <teck_kenneth@hotmail.com>
2024-11-14 16:37:27 +0100

    added solution for numbers > 3999

--- Proefexamen/dec_to_roman.py
+++ Proefexamen/dec_to_roman.py
@@ -3,7 +3,15 @@ def dec_to_roman(n:str):
     decimals = len (n)
     roman_number = ""
     start = 0
+    if int(n) == 4000:
+        return "MMMM"
+    if int(n) > 4000:
+        extra = "M" * (int(n) // 1000)
+        n = str(int(n) % 1000)
+        decimals = len (n)
+
     for i in range(len (n)):
+
         x = int(n[decimals -1])
         decimals = decimals - 1
         if x == 0: None
@@ -12,7 +20,9 @@ def dec_to_roman(n:str):
         elif x < 9: roman_number = roman[start+1] + roman[start] * (x-5) + roman_number
         elif x == 9: roman_number = roman[start] + roman[start+2] + roman_number
         start += 2
-        print (roman_number)
 
-dec_to_roman("18")
+    roman_number = extra + roman_number
+    return roman_number
+
+print(dec_to_roman("18008"))
 
