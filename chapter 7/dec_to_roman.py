01e41a41b3a8c628815dd6e03249ce5af3060040
bedoosd <teck_kenneth@hotmail.com>
2024-11-16 14:50:04 +0100

    adjusted so input is an integer instead of a string

--- chapter 7/dec_to_roman.py
+++ chapter 7/dec_to_roman.py
@@ -1,22 +1,22 @@
-def dec_to_roman(n:str):
-    roman = ["I", "V", "X", "L", "C", "D", "M" ]
+def dec_to_roman(n: int):
+    roman = ["I", "V", "X", "L", "C", "D", "M", "O", "K" ]
     roman_number = ""
     start = 0
-    extra = "M" * (int(n) // 1000) #solution for numbers > 3999
-    n = str(int(n) % 1000) #only needs to calculate everything beneath 1000
+
+    extra = "M" * (n // 1000) #solution for numbers > 3999
+    n = str(n % 1000) #only needs to calculate everything beneath 1000
     decimals = len (n)
 
-    for i in range(len (n)):
+    for i in range(decimals):
 
         x = int(n[decimals -1])
-        if x == 0: None
+        if x == 0: pass
         elif x < 4: roman_number = roman[start] * x + roman_number
         elif x == 4: roman_number = roman[start] + roman[start+1] + roman_number
         elif x < 9: roman_number = roman[start+1] + roman[start] * (x-5) + roman_number
         elif x == 9: roman_number = roman[start] + roman[start+2] + roman_number
-        start += 2
-        decimals -= 1
-    roman_number = extra + roman_number
-    return roman_number
+        start, decimals = start+2, decimals-1
 
-print(dec_to_roman("852"))
+    return extra + roman_number
+
+print (dec_to_roman(662))
