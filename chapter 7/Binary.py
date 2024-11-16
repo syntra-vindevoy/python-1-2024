c9735cd71bd05145444b974daec6db31139d54d9
bedoosd <teck_kenneth@hotmail.com>
2024-11-16 13:18:14 +0100

    added a secondary solution using recursion + print for wrong input

--- chapter 7/Binary.py
+++ chapter 7/Binary.py
@@ -1,16 +1,44 @@
-import math
-def dec_to_binary (n):
-    highest_exp= int(math.log(n, 2))
-    binary_code_string = ""
-    exp = highest_exp
-    while exp > 0:
-        if n >= 2 ** (exp-1):
+from math import log
+
+def dec_to_binary_loop (n:int):
+    if n <= 0 or not isinstance(n, int): return "Decimal number has to be a positive integer"
+
+    exp = int(log(n, 2)) #calculates the highest binary to start from
+    binary_code = ""
+    while exp >= 0:
+        if n >= 2 ** exp:
             binary= "1"
             n -= 2**exp
-        else:
-            binary= "0"
-        binary_code_string = binary_code_string + binary
-        exp -= 1
-    return binary_code_string
+        else: binary= "0"
 
-print(dec_to_binary(55546))
+        binary_code += binary
+        exp -= 1
+    return binary_code
+
+
+def dec_to_binary_rec (n:int, exp = 2, binary_code = ""):
+    if n <= 0 or not isinstance (n, int): return "Decimal number has to be a positive integer"
+
+    if (n % exp) == 0 :
+        binary_code = "0" + binary_code
+
+    else:
+        binary_code = "1" + binary_code
+        n -= (n % exp)
+        if n == 0: return binary_code
+
+    return dec_to_binary_rec (n, exp * 2, binary_code)
+
+if __name__ == "__main__":
+    assert dec_to_binary_loop(0) == "Decimal number has to be a positive integer"
+    assert dec_to_binary_loop(-8) == "Decimal number has to be a positive integer"
+    assert dec_to_binary_loop(8.4) == "Decimal number has to be a positive integer"
+    assert dec_to_binary_rec(18) == "10010"
+    assert dec_to_binary_loop(25) == "11001"
+    assert dec_to_binary_rec(-8) == "Decimal number has to be a positive integer"
+    assert dec_to_binary_rec(0) == "Decimal number has to be a positive integer"
+    assert dec_to_binary_rec(-56) == "Decimal number has to be a positive integer"
+    assert dec_to_binary_rec(4.9) == "Decimal number has to be a positive integer"
+    
+    print (dec_to_binary_loop(52))
+    print (dec_to_binary_rec(47))
