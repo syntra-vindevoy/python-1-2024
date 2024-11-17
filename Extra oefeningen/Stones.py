5cfc4d14c7a5c02a0c851e5deca0837edab1139b
Freekvera <veraghtertfreek@gmail.com>
2024-11-17 23:15:06 +0100

    Stones trachten te begrijpen

--- Extra oefeningen/Stones.py
+++ Extra oefeningen/Stones.py
@@ -1,1 +1,16 @@
-?
+from sortedcontainers import SortedDict
+
+def main():
+    weights = SortedDict()
+    stones = [1, 3, 9, 27]
+
+    for stone in stones:
+        for p in list(weights.keys()):
+            weights[stone + p] = [p, stone]
+            weights[stone - p] = [-p, stone]
+
+        weights[stone] = [stone]
+
+
+    print(weights)
+
