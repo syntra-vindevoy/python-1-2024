00da536cfdad740f44fe3e1c193ba549254a5015
Freekvera <veraghtertfreek@gmail.com>
2024-11-17 21:28:44 +0100

    Examen prep

--- H3EX3.py
+++
@@ -1,7 +0,0 @@
-def rectangle(char, width, height):
-    for i in range(height):
-        # Print a row with the character repeated for the width of the rectangle
-        print(char * width)
-
-# Example usage:
-rectangle('H', 5, 4)
--- H4PARAM.py
+++
@@ -1,6 +0,0 @@
-def divide(a,b):
-    return a / b
-print(divide(6, 2))
-print(divide(2,6))
-
-print(divide(a=6, b=3))
