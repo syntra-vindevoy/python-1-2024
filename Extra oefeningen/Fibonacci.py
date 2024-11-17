313e30105254c882bb900c3f9f96452c45215dee
Freekvera <veraghtertfreek@gmail.com>
2024-11-17 22:37:40 +0100

    Fibo no recursion

--- Extra oefeningen/Fibonacci.py
+++ Extra oefeningen/Fibonacci.py
@@ -1,4 +1,4 @@
-#recursion:
+#recursion
 def fibonacci(n):
     if n == 0:
         return 0
@@ -6,4 +6,27 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
-#itteration:
+
+#no recursion
+def fibo(n: int) -> int:
+    if n < 2:
+        return n
+
+    fibo_0 = 0
+    fibo_1 = 1
+
+    for _ in range(2, n):
+        fibo_0, fibo_1 = fibo_1, fibo_0 + fibo_1
+#New fibo_0 gets the old value of fibo_1.
+#New fibo_1 gets the sum of the old fibo_0 and fibo_1.
+    return fibo_0 + fibo_1
+
+assert fibo(0) == 0
+assert fibo(1) == 1
+assert fibo(2) == 1
+assert fibo(3) == 2
+assert fibo(4) == 3
+assert fibo(5) == 5
+assert fibo(6) == 8
+assert fibo(7) == 13
+assert fibo(8) == 21
