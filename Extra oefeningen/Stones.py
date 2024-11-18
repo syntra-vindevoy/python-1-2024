b1fab2e0b8d624a28c877b2466cdda3a5eef7081
Freekvera <veraghtertfreek@gmail.com>
2024-11-18 22:53:22 +0100

    Stones zonder import

--- Extra oefeningen/Stones.py
+++ Extra oefeningen/Stones.py
@@ -1,15 +1,18 @@
-from sortedcontainers import SortedDict
-
 def main():
-    weights = SortedDict()
-    stones = [1, 3, 9, 27]
+    weights = {}  # Use a regular dictionary
+    stones = [1, 3, 9, 27]  # List of stone values
 
-    for stone in stones:
-        for p in list(weights.keys()):
-            weights[stone + p] = [p, stone]
-            weights[stone - p] = [-p, stone]
+    for stone in stones:  # Loop through each stone in the list
+        # Make a list of current keys and sort them
+        keys = sorted(weights.keys())  # Get the keys from 'weights' and sort them
+        for p in keys:  # Loop through each key in the sorted list
+            weights[stone + p] = [p, stone]  # Add new entry for stone + p
+            weights[stone - p] = [-p, stone]  # Add new entry for stone - p
 
-        weights[stone] = [stone]
+        weights[stone] = [stone]  # Add the stone itself to the dictionary with its own value
 
-    print(weights)
+    # To print in sorted order, we sort the keys of the dictionary
+    sorted_weights = {k: weights[k] for k in sorted(weights)}  # Sort the dictionary by keys
+    print(sorted_weights)  # Print the sorted dictionary
 
+main()  # Call the main function to execute
