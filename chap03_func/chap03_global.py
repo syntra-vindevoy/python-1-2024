3cfe46ed9e405e80aaeffd2ca951cae29278c130
Thomas <thomas.meersschaut@gmail.com>
2024-11-19 15:00:04 +0100

    Fix typo in comment regarding global variable assignment
    Corrected a typographical error in a comment within the function `f()`, changing "gere" to "here" for clarity. This improves the readability and understanding of the comment for future developers.

--- chap03_func/chap03_global.py
+++ chap03_func/chap03_global.py
@@ -5,7 +5,7 @@ from global_strings import APPLICATION_NAME
 a = 2
 
 def f():
-    global a                    #Not possible to assign value gere
+    global a                    #Not possible to assign value here
 
     a = 3
 
