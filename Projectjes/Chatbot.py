eff14af867fc91f865c2bbd56fcd2f035c228cb0
Freekvera <veraghtertfreek@gmail.com>
2024-11-19 17:22:46 +0100

    Crossing bridge global solution

---
+++ Projectjes/Chatbot.py
@@ -0,0 +1,13 @@
+bot_name: str = 'Dave'
+print(f'Hello! My name is {bot_name}, how can I help you?')
+
+def bot():
+    while True:
+        user_input: str = input('You: ').lower()
+        if user_input == 'quit':
+            print('See you later')  # Move this before the break
+            break
+        else:
+            print(f'{bot_name}: I heard you say "{user_input}"')  # Example response
+
+bot()
