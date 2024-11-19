90f4deeb658e08462fb02c362b6191c50d2c9e37
bedoosd <teck_kenneth@hotmail.com>
2024-11-19 16:08:33 +0100

    modified to work with numbers smaller then 2000

--- chapter 7/decimal_to_other_systems.py
+++ chapter 7/decimal_to_other_systems.py
@@ -1,12 +1,13 @@
 from hexadecimal import dec_to_hexadecimal
 from Binary import dec_to_binary_loop
 from dec_to_roman import higher_roman_numbers
+from dec_to_roman import dec_to_roman
 
 def main (n):
     print (f"Decimal = {n}")
     print (f"Binary = {dec_to_binary_loop(n)}")
     print (f"Hexadecimal = {dec_to_hexadecimal(n)}")
-    print (f"Roman = {higher_roman_numbers(n)}")
+    print (f"Roman = {higher_roman_numbers(n)}" if n > 3999 else f"Roman = {dec_to_roman(n)}")
 
 if __name__ == '__main__':
-    main (254255)
+    main (4255)
