c57448618f74cbfd6a8286f86d93ca4536f6f011
Thomas <thomas.meersschaut@gmail.com>
2024-11-26 18:53:01 +0100

    Add tolerance parameter to mysqrt function
    Refactored mysqrt to include an optional tolerance parameter. This enables more precise control over the stopping condition for the loop. Enhanced the function to maintain better accuracy in results.

--- chap07_interation/chap07_ex1.py
+++ chap07_interation/chap07_ex1.py
@@ -11,3 +11,15 @@ def mysqrt(number: float) -> float:
         x = y
 
 mysqrt(120)
+
+
+
+
+def mysqrt(number: float, tolerance: float = 1e-10) -> float:
+    x = number
+
+    while True:
+        y = (x + (number / x)) / 2
+        if abs(y - x) < tolerance:  # Compare with a small tolerance
+            return y
+        x = y
