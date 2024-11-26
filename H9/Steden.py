d33f94f7d4db51f1d4590c1457551bd460b4b698
Freekvera <veraghtertfreek@gmail.com>
2024-11-26 21:58:42 +0100

    List index shift fix

--- H9/Steden.py
+++ H9/Steden.py
@@ -1,7 +1,7 @@
 Steden = ["Oudenaarde", "Erpe-mere", "Deinze", "Zottegem", "Wetteren", "Gent", "Aalst", "Ninove", "Aalter", "Opwijk", "Mariakerke"]
 
-for name in Steden[:]:
-    if name[0].lower() in "aeiou":
-        Steden.remove(name)
+for stad in Steden[:]:
+    if stad[0].lower() in "aeiou":
+        Steden.remove(stad)
 
 print(Steden)
