5d6ac37b86c5ab592703781cbf8c89a5e07dc78d
Synix1997 <deruyck.sebastien97@gmail.com>
2024-11-26 21:25:38 +0100

    Improved function by putting code on 1 line

--- Chapter 9/9.15.2_anagram.py
+++ Chapter 9/9.15.2_anagram.py
@@ -2,8 +2,7 @@
 #Write a function called is_anagram that takes two strings and returns True if they are anagrams.
 
 def is_anagram(word_1: str, word_2: str) -> bool:
-    if ''.join(sorted(word_1)) == ''.join(sorted(word_2)): return True
-    else: return False
+    return ''.join(sorted(word_1)) == ''.join(sorted(word_2))
 
 assert is_anagram("pot", "top") == True
 assert is_anagram("stop", "top") == False
