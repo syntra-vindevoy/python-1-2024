81471a818dde62e242e2125889b325800b08e9fd
Synix1997 <deruyck.sebastien97@gmail.com>
2024-11-26 21:24:10 +0100

    Finished exercise anagram

---
+++ Chapter 9/9.15.2_anagram.py
@@ -0,0 +1,10 @@
+#Two words are anagrams if you can rearrange the letters from one to spell the other. For example, tops is an anagram of stop.
+#Write a function called is_anagram that takes two strings and returns True if they are anagrams.
+
+def is_anagram(word_1: str, word_2: str) -> bool:
+    if ''.join(sorted(word_1)) == ''.join(sorted(word_2)): return True
+    else: return False
+
+assert is_anagram("pot", "top") == True
+assert is_anagram("stop", "top") == False
+assert is_anagram("stoep", "poets") == True
