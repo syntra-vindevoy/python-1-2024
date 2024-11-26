133f7ff125fd3a46e3d7caaa643bb23fc42588d6
Thomas <thomas.meersschaut@gmail.com>
2024-11-26 21:58:44 +0100

    Add script to remove cities starting with a vowel
    Created a new script, chap09_ex_cities2.py, which initializes a list of cities and removes those whose names start with a vowel. The script then prints the list before and after the removal operation.

---
+++ chap09_lists/chap09_ex_cities2.py
@@ -0,0 +1,9 @@
+#remove all cities which start with a vowel
+steden = ["oudenaarde", "zottegem", "sint-niklaas", "kortrijk", "deinze", "gent", "lede", "erpe-mere", "oostakker", "wetteren", "aalst", "ninove", "aalter", "opwijk"]
+print(steden)
+
+for stad in steden(,, -1):
+    if stad[0] in "aeiouyAEIOUY":
+        steden.remove(stad)
+
+print(steden)
