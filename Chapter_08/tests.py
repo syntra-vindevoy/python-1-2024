882c399e412da7f3766aea203318a9b45b83b3ea
GeoffreyCrapoen <crapoen.geoffrey@gmail.com>
2024-11-27 18:49:51 +0100

    Added test file for testing the content in chapter 8

---
+++ Chapter_08/tests.py
@@ -0,0 +1,19 @@
+#
+# naam = 'Geoffrey'
+# letter = naam[-1]
+# print(letter)
+# print(len(letter))
+
+import os
+import subprocess
+import requests
+
+# if not os.path.exists('pg345.txt'):
+#     subprocess.run(['wget', 'https://www.gutenberg.org/cache/epub/345/pg345.txt'])
+
+if not os.path.exists('pg345.txt'):
+    url = 'https://www.gutenberg.org/cache/epub/345/pg345.txt'
+    response = requests.get(url)
+    with open('pg345.txt', 'wb') as f:
+        f.write(response.content)
+
