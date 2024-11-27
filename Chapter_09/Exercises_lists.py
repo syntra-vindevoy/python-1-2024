5360de415680afa65b7067a433b5e4397ebcde13
GeoffreyCrapoen <crapoen.geoffrey@gmail.com>
2024-11-27 20:58:01 +0100

    Made directory for chapter 9 + made some of the exercises of thinkpyhton V2

---
+++ Chapter_09/Exercises_lists.py
@@ -0,0 +1,27 @@
+# exercise 10.1 from book thinkpython
+
+t= [[1,2], [4,5,6], [3]]
+def nested_sum(list):
+    return sum(sum(sublist) for sublist in list)
+
+print(nested_sum(t))
+print (t[2])
+
+# exercise 10.2 from book thinkpython
+
+t2 = [1,2,3]
+def cumsum(list2):
+    result = []
+    current_sum = 0
+    for num in list2:
+        current_sum += num
+        result.append(current_sum)
+    return result
+print(cumsum(t2))
+
+# exercise 10.3 from book thinkpython
+
+t3 = [1, 2, 3, 4]
+def middle(list3):
+    return list3 [1:-1]
+print(middle(t3))
