4535b676521b803a31680092ee96e9e1a3fdce58
Thomas <thomas.meersschaut@gmail.com>
2024-11-28 19:38:24 +0100

    Add vowel removal filter to 'dorpen' list.
    Introduced a filtering mechanism to remove names starting with a vowel from the 'dorpen' list. This change parallels the existing functionality applied to the 'steden' and 'cities' lists, ensuring consistent behavior across all lists.

--- chap09_lists/chap09_ex_cities2.py
+++ chap09_lists/chap09_ex_cities2.py
@@ -1,6 +1,7 @@
 #remove all cities which start with a vowel
 steden = ["oudenaarde", "zottegem", "sint-niklaas", "kortrijk", "deinze", "gent", "lede", "erpe-mere", "oostakker", "wetteren", "aalst", "ninove", "aalter", "opwijk"]
 cities = ["oudenaarde", "zottegem", "sint-niklaas", "kortrijk", "deinze", "gent", "lede", "erpe-mere", "oostakker", "wetteren", "aalst", "ninove", "aalter", "opwijk"]
+dorpen = ["oudenaarde", "zottegem", "sint-niklaas", "kortrijk", "deinze", "gent", "lede", "erpe-mere", "oostakker", "wetteren", "aalst", "ninove", "aalter", "opwijk"]
 print(steden)
 
 for stad in steden[::-1]:               #When using a FOR on a list and you remove an element, the elements that come after move index number
@@ -19,3 +20,7 @@ for city in cities[::-1]:
 
 print(cities)
 
+
+print(dorpen)
+dorpen = [dorp for dorp in dorpen if dorp[0].lower() not in "aeiouy"]
+print(dorpen)
