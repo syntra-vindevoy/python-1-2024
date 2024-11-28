632c9d0b0ee3c2c863706055dff5f1cf45ea121f
Thomas <thomas.meersschaut@gmail.com>
2024-11-28 20:13:48 +0100

    Add performance testing for new anagram methods
    Introduced new methods (method_3, method_4, and method_5) to refine the process of finding anagrams by utilizing different filtering techniques based on the presence of specific letters and word lengths. These changes aim to enhance the performance of anagram detection by comparing execution times across different methods. The test outputs the time taken for each method, highlighting potential efficiency improvements.

--- chap09_lists/chap09_ex15_2.py
+++ chap09_lists/chap09_ex15_2.py
@@ -1,6 +1,10 @@
 from datetime import datetime
-
 from chap09_is_anagram import is_anagram
+
+###Classic way, but bad way, because you have to close the file at the end
+#f = open("words.txt", "r")
+#words = f.read().split("\n")
+#f.close()
 
 #Correct way, because the context manager "with" already closes the file for you at the end
 with open("words.txt", "r") as f:
@@ -21,26 +25,62 @@ def method_1():
 
     return anagrams
 
+
 def method_2():
     return [word for word in words if is_anagram(word, "takes")]
 
 
-###Classic way, but bad way, because you have to close the file at the end
-#f = open("words.txt", "r")
-#words = f.read().split("\n")
-#f.close()
+def method_3():
+    w = [word for word in words if "t" in word]
+    w = [word for word in w if "k" in word]
+    w = [word for word in w if "s" in word]
+
+    return [word for word in w if is_anagram(word, "takes")]
+
+
+def method_4():
+    w = [word for word in words if len(word) == 5]
+
+    return [word for word in w if is_anagram(word, "takes")]
+
+
+def method_5():
+    w = [word for word in words if len(word) == 5]
+    w = [word for word in w if "t" in word]
+    w = [word for word in w if "k" in word]
+    w = [word for word in w if "s" in word]
+
+    return [word for word in w if is_anagram(word, "takes")]
+
 
 if __name__ == "__main__":
     start = datetime.now()
     for _ in range(100):
         method_1()
+    end = datetime.now()
+    print(end - start)
 
+
+    start = datetime.now()
+    for _ in range(100):
+        method_2()
     end = datetime.now()
     print(end - start)
 
     start = datetime.now()
     for _ in range(100):
-        method_2()
+        method_3()
+    end = datetime.now()
+    print(end - start)
 
+    start = datetime.now()
+    for _ in range(100):
+        method_4()
+    end = datetime.now()
+    print(end - start)
+
+    start = datetime.now()
+    for _ in range(100):
+        method_5()
     end = datetime.now()
     print(end - start)
