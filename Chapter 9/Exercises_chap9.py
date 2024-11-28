9196b846797f64b19971a11957d0746bb2bb7582
Synix1997 <deruyck.sebastien97@gmail.com>
2024-11-28 19:02:48 +0100

    extra oefening steden afgewerkt + bijgewerkt in de klas

--- Chapter 9/Exercises_chap9.py
+++ Chapter 9/Exercises_chap9.py
@@ -2,4 +2,12 @@
 steden = ["Oudenaarde", "Zottegem", "Sint-Niklaas", "Kortrijk", "Deinze", "Gent", "Lede", "Erpe-Mere", "Wetteren", "Aalst", "Ninove", "Aalter", "Opwijk", ]
 vowels = ["a", "e", "i", "o", "u", "y"]
 
-steden.remove()
+for stad in steden[::-1]:
+    print("Ik controleer ", stad)
+    begin = stad[0].lower()
+
+    if begin in vowels:
+        print("Ik verwijder ", stad)
+        steden.remove(stad)
+
+print(f"De overgebleven steden zijn {steden}")
