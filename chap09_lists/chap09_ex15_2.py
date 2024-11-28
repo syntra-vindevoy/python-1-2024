d19b4a0503c787e6f765d96b92727ac46ba338c4
Thomas <thomas.meersschaut@gmail.com>
2024-11-28 20:05:36 +0100

    Refactor anagram detection with performance comparison.
    Introduce two methods for detecting anagrams and compare their execution time by running each method 100 times. This change helps evaluate the performance difference between a for-loop based approach and a list comprehension approach.

--- chap09_lists/chap09_ex15_2.py
+++ chap09_lists/chap09_ex15_2.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from chap09_is_anagram import is_anagram
 
 #Correct way, because the context manager "with" already closes the file for you at the end
@@ -11,10 +13,16 @@ for word in words:
     if is_anagram(word, "takes"):
         print(word)
 
-anagrams = [word for word in words if is_anagram(word, "takes")]
-print(anagrams)
+def method_1():
+    anagrams = []
+    for word in words:
+        if is_anagram(word, "takes"):
+            anagrams.append(word)
 
+    return anagrams
 
+def method_2():
+    return [word for word in words if is_anagram(word, "takes")]
 
 
 ###Classic way, but bad way, because you have to close the file at the end
@@ -22,3 +30,17 @@ print(anagrams)
 #words = f.read().split("\n")
 #f.close()
 
+if __name__ == "__main__":
+    start = datetime.now()
+    for _ in range(100):
+        method_1()
+
+    end = datetime.now()
+    print(end - start)
+
+    start = datetime.now()
+    for _ in range(100):
+        method_2()
+
+    end = datetime.now()
+    print(end - start)
