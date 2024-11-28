f456b1972f23cf7052c95dee3e8fa51fb0846bcf
bedoosd <teck_kenneth@hotmail.com>
2024-11-28 16:32:31 +0100

    more exercises book 2 chapter 10

--- chapter 9/exercises_book2_chapter10.py
+++ chapter 9/exercises_book2_chapter10.py
@@ -12,3 +12,30 @@ def cum_sum(numbers):
     return t
 
 print (cum_sum ([1,3,4,5]))
+
+
+def middle (numbers):
+    t = numbers[1:-1]
+    return t
+
+print (middle ([1,2,3,4,5,6]))
+
+def chop(numbers):
+    numbers.remove(numbers[0])
+    numbers.remove(numbers[-1])
+    return None
+print (chop ([1,2,3,4,5]))
+
+
+def is_sorted(numbers):
+    return sorted(numbers) == numbers
+
+print (is_sorted([1,3,4,5]))
+print (is_sorted(["a", "d", "c"]))
+
+def has_duplicates(n):
+    test = False
+    for i in range (len(n)):
+        if n[i] in n[:i:]: return True
+    return test
+print (has_duplicates([1, 2, 3, "g", 4, 5, "a", "g" ]))
