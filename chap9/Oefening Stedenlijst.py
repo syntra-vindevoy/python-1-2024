b02fc651d7079415c10fd8b511412ba7ba03be25
Alvaro Avelar <alvaro1alexander@gmail.com>
2024-11-28 18:47:21 +0100

    Stedenlijst

---
+++ chap9/Oefening Stedenlijst.py
@@ -0,0 +1,6 @@
+# Verwijder uit de lijst alle steden die beginnen met een klinker. Pop of remove gebruiken.
+
+cities = ['oudenaarde', 'zottegem', 'sint-niklaas', 'kortrijk', 'deinze', 'gent', 'wetteren', 'erpe-mere', 'aalst', 'ninove', 'aalter', 'opwijk']
+
+vowels = 'a', 'e', 'i', 'o', 'u'
+
--- chap9/Stedenlijst.py
+++
@@ -1,3 +0,0 @@
-# Verwijder uit de lijst alle steden die beginnen met een klinker. Pop of remove gebruiken.
-
-steden = ['oudenaarde', 'zottegem', 'sint-niklaas', 'kortrijk', 'deinze', 'gent', 'wetteren', 'erpe-mere', 'aalst', 'ninove', 'aalter', 'opwijk']
