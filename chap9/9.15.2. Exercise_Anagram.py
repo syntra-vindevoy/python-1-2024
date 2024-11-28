39ad79de116a08e4127aeec0de56737dce1d00c2
Alvaro Avelar <alvaro1alexander@gmail.com>
2024-11-28 20:39:56 +0100

    Commit is_anagram with search function.

--- chap9/9.15.2. Exercise_Anagram.py
+++ chap9/9.15.2. Exercise_Anagram.py
@@ -1,2 +1,16 @@
-def is_anagram(a: list , b: list) -> bool:
-    return sorted(a) == sorted(b)
+def is_anagram(word1: str , word2: str) -> str:
+    return sorted(word1) == sorted(word2)
+
+with open("words.txt", "r") as f:
+    words = f.read().split("\n")
+
+def method_5():
+    w = [word for word in words if word == len(word)]
+    w = [word for word in words if "t" in word]
+    w = [word for word in words if "a" in word]
+    w = [word for word in words if "k" in word]
+    w = [word for word in words if "e" in word]
+    w = [word for word in words if "s" in word]
+
+    return [word for word in w if is_anagram(word, "takes")]
+print(method_5())
--- chap9/9.15.2. Exercise_Anagram_Word.py
+++ chap9/9.15.2. Exercise_Anagram_Word.py
@@ -1,9 +1,3 @@
 with open("words.txt", "r") as f:
     words = f.read().split("\n")
 
-print(words)
-print(type(words))
-
-f = open("word.txt", "r")
-words = f.read().split("\n")
-f.close()
