22a0c81672ff3cb5da42af65b5d3496e2728ef07
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-11-28 23:12:06 +0100

    Reverse iteration over cities in list_exercises.py
    The loop now iterates over the 'steden' list in reverse order. This change ensures that cities are checked starting from the last to the first, which may be necessary for contextual reasons or analysis order.

--- yves/list_exercises.py
+++ yves/list_exercises.py
@@ -15,12 +15,14 @@ assert not is_voodoo("oov")
 steden = ["Brussel", "Oudenaarde", "Zottegem", "Opwijk", "Sint-Niklaas", "Kortrijk", "Deinze", "Gent", "Aalst",
           "Erpe-Mere", "Ninove"]
 
-for stad in steden[::-1]:
-    print("Ik controleer", stad)
-    begin = stad[0].lower()
+# for stad in steden[::-1]:
+#     print("Ik controleer", stad)
+#     begin = stad[0].lower()
+#
+#     if begin in "aeuioy":
+#         print("Ik verwijder ", stad)
+#         steden.remove(stad)
 
-    if begin in "aeuioy":
-        print("Ik verwijder ", stad)
-        steden.remove(stad)
+steden = [stad for stad in steden if stad[0].lower() not in "aeuioy"]
 
 print(steden)
