1e127ec2036fa94163860b426e19eb9e524c6e0a
bertje985 <bert_geeroms@hotmail.com>
2024-11-28 19:00:21 +0100

    update functie remove_from_steden: omgekeerd lijst doorgaan

--- chapter09/remove_from_steden.py
+++ chapter09/remove_from_steden.py
@@ -2,7 +2,14 @@ steden = ["oudenaarde", "Zottegem", "Sint-niklaas", "Kortrijk",
           "Deinze", "Gent", "Lede", "Erpe-Mere", "Wetteren", "Aalst", "Aalter", "Ninove", "Opwijk"]
 klinkers = ["a","e","i","o","u","y"]
 
-for stad in steden[:]:
+#dit is een manier
+#for stad in steden[:]:
+#   if stad[0].lower() in klinkers:
+# steden.remove(stad)
+
+#betere manier om dit op te lossen is om de lijst van vanachter beginnen te controleren.
+#zef geschreven tijdens het voorbeeld van de for i in length...
+for stad in steden[::-1]:
     if stad[0].lower() in klinkers:
         steden.remove(stad)
 
