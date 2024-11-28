283a5517c45373de77d73132280451a1807fb930
bertje985 <bert_geeroms@hotmail.com>
2024-11-28 19:07:35 +0100

    update functie remove_from_steden: toevoegen list comprehension

--- chapter09/remove_from_steden.py
+++ chapter09/remove_from_steden.py
@@ -8,9 +8,13 @@ klinkers = ["a","e","i","o","u","y"]
 # steden.remove(stad)
 
 #betere manier om dit op te lossen is om de lijst van vanachter beginnen te controleren.
-#zef geschreven tijdens het voorbeeld van de for i in length...
+#zelf geschreven tijdens het voorbeeld van de for i in length...
 for stad in steden[::-1]:
     if stad[0].lower() in klinkers:
         steden.remove(stad)
+
+#hieronder wordt gebruik gemaakt van list comprehension
+#maakt tijdelijk een nieuwe lijst aan op deze manier:
+steden = [stad for stad in steden if stad[0].lower() not in klinkers]
 
 print(steden)
