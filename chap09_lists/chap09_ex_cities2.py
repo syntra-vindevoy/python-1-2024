6fa9b8ed7c9f4d1b90a6b7ac2ca4609ea287c00b
Thomas <thomas.meersschaut@gmail.com>
2024-11-28 19:01:51 +0100

    Refactor city removal logic for clarity and accuracy.
    Introduced a new list for cities and refined the loop to ensure correct index handling by iterating in reverse. Simplified city removal conditions by consistently converting the first character to lowercase.

--- chap09_lists/chap09_ex_cities2.py
+++ chap09_lists/chap09_ex_cities2.py
@@ -1,9 +1,21 @@
 #remove all cities which start with a vowel
 steden = ["oudenaarde", "zottegem", "sint-niklaas", "kortrijk", "deinze", "gent", "lede", "erpe-mere", "oostakker", "wetteren", "aalst", "ninove", "aalter", "opwijk"]
+cities = ["oudenaarde", "zottegem", "sint-niklaas", "kortrijk", "deinze", "gent", "lede", "erpe-mere", "oostakker", "wetteren", "aalst", "ninove", "aalter", "opwijk"]
 print(steden)
 
-for stad in steden(,, -1):
+for stad in steden[::-1]:               #When using a FOR on a list and you remove an element, the elements that come after move index number
     if stad[0] in "aeiouyAEIOUY":
         steden.remove(stad)
 
 print(steden)
+
+
+print(cities)
+
+for city in cities[::-1]:
+    begin = city[0].lower()
+    if begin in "aeiouy":
+        cities.remove(city)
+
+print(cities)
+
