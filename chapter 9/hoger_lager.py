194ec05f97f39e71acc3a495f8a3cfc190fe4567
bedoosd <teck_kenneth@hotmail.com>
2024-11-30 00:16:26 +0100

    hoger lager andere methode

--- chapter 9/hoger_lager.py
+++ chapter 9/hoger_lager.py
@@ -1,10 +1,10 @@
 import random
 import statistics
-
+"""
 def hoger_lager(min, max):
     x = random.randint(min, max)
     y = random.randint(min, max)
-    count = 0
+    count = 1
     while x != y:
         print (f"probeer: {y}")
         count += 1
@@ -22,8 +22,44 @@ hoger_lager(0, 10000)
 
 result = []
 for i in range(1000):
+    result.append(hoger_lager(0, 1000))
+
+print (max(result))
+print (min(result))
+print (statistics.mean(result))"""
+
+import random
+
+def hoger_lager(min, max):
+    x = random.randint(min, max)
+    y = (min + max) // 2 #universeler dan max // 2
+    count = 1
+    min_next = min
+    max_next = max
+
+    while x != y:
+        print(f"probeer: {y}")
+        if y > x:
+            print("lager!")
+            max_next = y - 1  # -1 voor afronding op te vangen
+        else:
+            print("hoger!")
+            min_next = y + 1  # +1 voor afronding op te vangen
+
+        y = (min_next + max_next) // 2
+        count += 1
+
+    print(f"Het heeft {count} beurten geduurd om {x} te vinden")
+    return count
+
+hoger_lager(0, 10000)
+
+result = []
+for i in range(100000):
     result.append(hoger_lager(0, 10000))
 
 print (max(result))
 print (min(result))
 print (statistics.mean(result))
+
+
