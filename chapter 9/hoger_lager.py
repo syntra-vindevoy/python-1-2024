9ce7164c53857c33d9a09213603ec3a2a9a3ee54
bedoosd <teck_kenneth@hotmail.com>
2024-11-30 11:13:38 +0100

    hoger lager. Beide methodes + extra tekst

--- chapter 9/hoger_lager.py
+++ chapter 9/hoger_lager.py
@@ -1,34 +1,33 @@
 import random
 import statistics
-"""
-def hoger_lager(min, max):
-    x = random.randint(min, max)
-    y = random.randint(min, max)
-    count = 1
-    while x != y:
-        print (f"probeer: {y}")
-        count += 1
-        if y > x:
-            max = y
-            print ("lager!")
-        else:
-            min = y
-            print ("hoger!")
-        y = random.randint(min, max)
-    print (f"Het heeft {count} beurten geduurd om {x} te vinden")
-    return count
-hoger_lager(0, 10000)
 
+# def hoger_lager(min, max):
+#     x = random.randint(min, max)
+#     y = random.randint(min, max)
+#     count = 1
+#     while x != y:
+#         print (f"probeer: {y}")
+#         count += 1
+#         if y > x:
+#             max = y
+#             print ("lager!")
+#         else:
+#             min = y
+#             print ("hoger!")
+#         y = random.randint(min, max)
+#     print (f"Het heeft {count} beurten geduurd om {x} te vinden")
+#     return count
+# hoger_lager(0, 10000)
+#
+#
+# result = []
+# for i in range(1000):
+#     result.append(hoger_lager(0, 10000))
+#
+# print (max(result))
+# print (min(result))
+# print (statistics.mean(result))
 
-result = []
-for i in range(1000):
-    result.append(hoger_lager(0, 1000))
-
-print (max(result))
-print (min(result))
-print (statistics.mean(result))"""
-
-import random
 
 def hoger_lager(min, max):
     x = random.randint(min, max)
@@ -38,28 +37,29 @@ def hoger_lager(min, max):
     max_next = max
 
     while x != y:
-        print(f"probeer: {y}")
+        #print(f"poging {count}: probeer: {y}")
         if y > x:
-            print("lager!")
-            max_next = y - 1  # -1 voor afronding op te vangen
+            #print("lager!")
+            max_next = y
         else:
-            print("hoger!")
-            min_next = y + 1  # +1 voor afronding op te vangen
-
+            #print("hoger!")
+            min_next = y + 1
+            #if y % 2 != 0: # doet er enkel +1 bij bij oneven getallen, maakt geen verschil in uitkomst
+            #    min_next = y + 1
+            #else: min_next = y
         y = (min_next + max_next) // 2
         count += 1
 
-    print(f"Het heeft {count} beurten geduurd om {x} te vinden")
+    #print(f"Het heeft {count} beurten geduurd om {x} te vinden")
     return count
 
 hoger_lager(0, 10000)
 
 result = []
-for i in range(100000):
+for i in range(10000):
     result.append(hoger_lager(0, 10000))
 
 print (max(result))
 print (min(result))
 print (statistics.mean(result))
-
 
