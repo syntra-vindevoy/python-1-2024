4b327dcb1ef42dd446625b2deef1503c52fa2de6
bucofski <tom_vandevyver@telenet.be>
2024-12-01 15:35:51 +0100

    Fix typo and update number range in search function
    Corrected a typo in the function name from 'number_seacher' to 'number_searcher'. Also, adjusted the random number generation and maximum number range from 10,000 to 100,000,000 for broader coverage in the search algorithm. Added print statements to facilitate debugging and tracking of search steps.

--- Chap_9/Chap9_extra.py
+++ Chap_9/Chap9_extra.py
@@ -28,29 +28,36 @@ from datetime import datetime
 # target_number = int(input("Enter the target number (1 to 10000): "))
 # final_value = number_seacher(target_number, max_number, 0)
 # print("Final value:", final_value)  # Print the final value returned by the function
+import random
+
+# Generate a random number between 1 and 10000
+random_number = random.randint(1, 100000000)
+
+print("Random number generated:", random_number)
+
 
 start = datetime.now()
 
-def number_seacher(target_number, search_min, search_max, counter=0):
+def number_searcher(target_number, search_min, search_max, counter=0):
     counter += 1
     search = (search_min + search_max) // 2
 
     if search == target_number:
         print(f"Target found in {counter} steps!")
-        #print(f"Target found in {search} steps!")
+        print(f"Target found in {search} steps!")
         return search
     elif search < target_number:
-        #print(f"Step {counter}: {search} is lower than {target_number}")
-        return number_seacher(target_number, search + 1, search_max, counter)
+        print(f"Step {counter}: {search} is lower than {target_number}")
+        return number_searcher(target_number, search + 1, search_max, counter)
     else:
-        #print(f"Step {counter}: {search} is higher than {target_number}")
-        return number_seacher(target_number, search_min, search - 1, counter)
+        print(f"Step {counter}: {search} is higher than {target_number}")
+        return number_searcher(target_number, search_min, search - 1, counter)
 
 
 # Input and function call
-max_number = 10000
-target_number = #int(input("Enter the target number (1 to 10000): "))
-final_value = number_seacher(target_number, 1, max_number)
+max_number = 100000000
+target_number = random_number #int(input("Enter the target number (1 to 10000): "))
+final_value = number_searcher(target_number, 1, max_number)
 print("Final value:", final_value)
 
 end = datetime.now()
