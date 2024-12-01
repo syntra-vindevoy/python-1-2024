8268582c3c3a2f8175a26a0a1c41cab3407faa80
mvdholen <marijnvandenholen@gmail.com>
2024-12-01 22:51:22 +0100

    refactor PostgreSQL connection handling and update function signatures to use cursor parameter

--- code_snippets/postgresql/connection.py
+++ code_snippets/postgresql/connection.py
@@ -1,60 +1,53 @@
 import psycopg2
 import os
 
-# Establish a connection to the database
-conn = psycopg2.connect(
-    dbname="test",
-    user="postgres",
-    password=os.getenv("DB_PASSWORD"),
-    # in bash terminal: export DB_PASSWORD="your_password"
-    host="127.0.0.1",
-    port="5432"
-)
-
-
-
-
-cur = conn.cursor()
-
-def get_column_names_and_types():
-    cur.execute("""
+def get_column_names_and_types(cursor):
+    cursor.execute("""
         SELECT column_name, data_type
         FROM information_schema.columns
         WHERE table_name = 'test2'
     """)
-
-    for column in cur.fetchall():
+    for column in cursor.fetchall():
         print(column)
 
-def show_table_content():
-    cur.execute("SELECT * FROM test2")
-    rows = cur.fetchall()
+def show_table_content(cursor):
+    cursor.execute("SELECT * FROM test2")
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-def get_table_content(table_name:str="test2"):
-    cur.execute(f"SELECT * FROM {table_name}")
-    rows = cur.fetchall()
+def get_table_content(cursor, table_name:str="test2"):
+    cursor.execute(f"SELECT * FROM {table_name}")
+    rows = cursor.fetchall()
     return rows
 
-def rename_column(old_name:str,new_name:str,table_name:str):
-    cur.execute(f"""
+def rename_column(cursor, old_name:str, new_name:str, table_name:str):
+    cursor.execute(f"""
         ALTER TABLE {table_name}
         RENAME COLUMN {old_name} TO {new_name}
     """)
-    conn.commit()
+    cursor.connection.commit()
 
 if __name__ == "__main__":
-    '''
-    get_column_names_and_types()
-    show_table_content()
-    '''
-    table_content = get_table_content('test2') 
-    for row in table_content:
-        id,voornaam,achternaam = row
-        print(id)
-        print(voornaam)
-        print(achternaam)
 
-cur.close()
-conn.close()
+    db_params = {
+        "dbname": "test",
+        "user": "postgres",
+        "password": os.getenv("DB_PASSWORD"),
+        # enter your password in the environment variables
+        # bash: export DB_PASSWORD="your_password"
+        "host": "127.0.0.1",
+        "port": "5432"
+    }
+
+    with psycopg2.connect(**db_params) as connection, \
+         connection.cursor() as cursor:
+        
+        table_content = get_table_content(cursor, 'test2')
+        for row in table_content:
+            id, voornaam, achternaam = row
+            print(id)
+            print(voornaam)
+            print(achternaam)
+
+    print("Connection closed.")
