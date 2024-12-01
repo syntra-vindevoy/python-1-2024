a6e3e611fd69cf1a11c1c472a33a04431b405869
mvdholen <marijnvandenholen@gmail.com>
2024-12-01 16:49:30 +0100

    oefening matching ages eerste aanzet

---
+++ lessen/chapter_09_lists/chapter_09_v2_oef9_moms_age.py
@@ -0,0 +1,31 @@
+opdracht = '''
+Exercise 9   Here’s another Car Talk Puzzler you can solve with a search (http://www.cartalk.com/content/puzzlers):
+“Recently I had a visit with my mom and we realized that the two digits that make up my age when reversed resulted in her age. For example, if she’s 73, I’m 37. We wondered how often this has happened over the years but we got sidetracked with other topics and we never came up with an answer.
+“When I got home I figured out that the digits of our ages have been reversible six times so far. I also figured out that if we’re lucky it would happen again in a few years, and if we’re really lucky it would happen one more time after that. In other words, it would have happened 8 times over all. So the question is, how old am I now?”
+
+Write a Python program that searches for solutions to this Puzzler. Hint: you might find the string method zfill useful.
+'''
+
+# age_me = x - y
+# age_mom = x
+# 6 times reversable until now
+# 8 times reversable in total
+
+solutions = []
+solution_count = 0
+minimum_age_for_birth = 10
+maximum_age_for_birth = 75
+maximum_age = 110
+
+solutions = []
+for my_age in range (0,maximum_age):
+    matching_ages = []
+    for moms_age_at_birth in range(minimum_age_for_birth,maximum_age_for_birth):
+        moms_age = my_age + moms_age_at_birth
+        if str(my_age).zfill(2) == str(moms_age).zfill(2)[::-1]:
+            matching_ages.append(my_age)
+    if matching_ages != []:
+        solutions.append(matching_ages)
+
+for solution in solutions:
+    print(solution)
