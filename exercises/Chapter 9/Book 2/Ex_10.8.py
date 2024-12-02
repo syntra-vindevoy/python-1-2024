e298d7190aaa9f89b40c6c76da705a14c72f8370
chiel <chiel.vansompel@gmail.com>
2024-12-02 19:54:44 +0100

    init with exercise description.

---
+++ exercises/Chapter 9/Book 2/Ex_10.8.py
@@ -0,0 +1,8 @@
+# Exercise 10.8.
+# This exercise pertains to the so-called Birthday Paradox,
+# which you can read about at http://en.wikipedia.org/wiki/Birthday_paradox.
+# If there are 23 students in your class, what are the chances that two of them
+# have the same birthday? You can estimate this probability by generating random
+# samples of 23 birthdays and checking for matches.
+# Hint: you can generate random birthdays with the randint function in the random module.
+
