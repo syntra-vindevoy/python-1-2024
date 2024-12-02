a4280f8025f9772e5ec0672d900c6d21aac293bf
chiel <chiel.vansompel@gmail.com>
2024-12-02 17:39:09 +0100

    Add `middle` function to extract list's middle elements
    This commit introduces a new function called `middle` that takes a list as input and returns a new list excluding the first and last elements. The code includes a main function to demonstrate its usage with an example list.

---
+++ exercises/Chapter 9/Book 2/Ex_10.3.py
@@ -0,0 +1,18 @@
+# Exercise 10.3
+# Write a function called middle that takes a list and returns a
+# new list that contains all but the first and last elements. For
+# example:
+# >>> t = [1, 2, 3, 4]
+# >>> middle(t)
+# [2, 3]
+
+t = [1, 2, 3, 4]
+
+def middle(t):
+    return t[1:-1]
+
+def main():
+    print(middle(t))
+
+if __name__ == "__main__":
+    main()
