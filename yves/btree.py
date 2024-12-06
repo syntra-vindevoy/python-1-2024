e80645d1f44aa1ef991990574b52ec27ae4955ad
Yves Vindevogel <vindevoy@users.noreply.github.com>
2024-12-06 15:17:35 +0100

    Add btree search and string methods in yves module
    Introduce btree search functions in btree.py for efficient searching within an integer range using recursive and iterative approaches. Additionally, implement string processing methods in ex_10_9.py to manipulate and benchmark word list transformations for performance evaluation.

---
+++ yves/btree.py
@@ -0,0 +1,36 @@
+import random
+
+def btree_search(to_find: int, lower: int, higher: int, attempts: int = 1):
+    middle = (lower + higher) // 2
+
+    if middle == to_find:
+        return attempts
+
+    if to_find < middle:
+        return btree_search(to_find, lower, middle - 1, attempts + 1)
+    else:
+        return btree_search(to_find, middle + 1, higher, attempts + 1)
+
+
+def btree_search_while(to_find: int, lower: int, higher: int):
+    attempts = 1
+
+    while True:
+        middle = (lower + higher) // 2
+
+        if middle == to_find:
+            return attempts
+
+        if to_find < middle:
+            higher = middle - 1
+        else:
+            lower = middle + 1
+
+        attempts += 1
+
+
+if __name__ == "__main__":
+    higher = 10000
+
+    print(max([btree_search_while(i + 1, 1, higher) for i in range(higher)]))
+    print(sum([btree_search_while(i + 1, 1, higher) for i in range(higher)]))
---
+++ yves/ex_10_9.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+def method_1(wrds: list[str]):
+    lst = []
+
+    for word in wrds:
+        lst += [word]
+
+    return lst
+
+def method_2(wrds: list[str]):
+    lst = []
+
+    for word in wrds:
+        lst.append(word.upper())
+
+    return lst
+
+def method_3(wrds: list[str]):
+    return [word.upper() for word in sorted(wrds)]
+
+def method_4(wrds: list[str]):
+    lst = []
+
+    for word in wrds:
+        lst.extend([word])
+
+    return lst
+
+
+def main():
+    times = 10000
+
+    with open("words.txt", "r") as f:
+        words = f.readlines()
+
+    # start = datetime.now()
+    # for _ in range(times):
+    #     method_1(words)
+    # end = datetime.now()
+    # print(end - start)
+
+    # start = datetime.now()
+    # for _ in range(times):
+    #     method_2(words)
+    # end = datetime.now()
+    # print(end - start)
+    #
+
+    start = datetime.now()
+    for _ in range(times):
+        method_3(words)
+    end = datetime.now()
+    print(end - start)
+
+    start = datetime.now()
+    for _ in range(times):
+        method_4(words)
+    end = datetime.now()
+    print(end - start)
+
+
+    dc = {word: len(word) for word in words}
+
+if __name__ == "__main__":
+    main()
