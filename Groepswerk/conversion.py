db4fb466f0b813144c47ee0af35e5ac48edd0d79
bucofski <tom_vandevyver@telenet.be>
2024-12-06 19:11:09 +0100

    Add PLC connection script and update file handling
    Introduce a new script to connect and download files via SSH and SFTP from a PLC using Paramiko. Update the conversion logic to handle floating-point conversions and change the input file from "for.dat" to "TDS.dat" for consistency with the new download path.

--- Groepswerk/conversion.py
+++ Groepswerk/conversion.py
@@ -1,7 +1,8 @@
+import struct
+
 # Open and read the file, splitting into words
-with open("for.dat", "r") as f:
+with open("TDS.dat", "r") as f:
     words = f.read().split("\n")
-    print(words)
 
 
 # Function to split strings into lists
@@ -17,6 +18,7 @@ def spilt_string(string):
 # Split the words into three lists
 list1 = spilt_string(words)
 
+
 def convert_and_split(string_list):
     processed_list = []
 
@@ -30,12 +32,20 @@ def convert_and_split(string_list):
             # Convert the ASCII hex to a character
             ascii_char = chr(int(ascii_hex, 16))
 
-            # Convert the decimal hex to an integerS
+            # Convert the decimal hex to an integer
             decimal_number = int(decimal_hex, 16)
             if len(str(decimal_number)) < 5:
                 decimal_number = str(decimal_number).zfill(5)
             # Append the processed components together with the rest of the sublist
-            processed_list.append([ascii_char + str(decimal_number)] + sublist[1:])
+
+            second_part = sublist[1]
+            if str(second_part) != "1" and str(second_part) != "0" :  # Ensure there is a second part
+                second_part = sublist[1]
+                int_value = int(second_part, 16)
+                # Pack the integer as bytes, then unpack as a float
+                float_value = struct.unpack('!f', struct.pack('!I', int_value))[0]
+                sublist[1] = float_value
+            processed_list.append([ascii_char + str(decimal_number)]  + sublist[1:] )
 
     return processed_list
 
@@ -43,8 +53,8 @@ def convert_and_split(string_list):
 # Process the list
 converted_list = convert_and_split(list1)
 
-
 if __name__ == "__main__":
     # Output processed list
+    print(words)
     print(list1)
     print(converted_list)
