0ba29da7168cecded7954a4e47612b6f1a3ee863
mvdholen <marijnvandenholen@gmail.com>
2024-12-08 20:12:03 +0100

    refactored presentation menu

--- code_snippets/3_tier/presentation/presentation.py
+++ code_snippets/3_tier/presentation/presentation.py
@@ -3,30 +3,34 @@ from time import sleep
 class Presentation:
     def __init__(self, business_logic):
         self.business_logic = business_logic
+        self.running = True
 
     def print_all_persons(self):
         persons = self.business_logic.get_all_persons()
         for person in persons:
             print(person.get_person_details())
 
-    def add_person(self, name:str):
-        self.business_logic.add_person(name)
+    def add_person(self):
+        self.business_logic.add_person(input("Enter the name of the person: "))
 
     def run(self):
-        running = True
-        while running:
+        while self.running:
+
             print("1. Print all persons")
             print("2. Add a person")
+            print('q, Quit')
+
             choice = input("Enter your choice: ")
 
-            if choice == '1': self.print_all_persons()
-            elif choice == '2':
-                name = input("Enter the name of the person: ")
-                self.add_person(name)
-            elif choice == 'q':
-                running = False
-            else:
-                print("Invalid choice")
+            actions = {
+                '1': self.print_all_persons,
+                '2': self.add_person,
+                'q': lambda: setattr(self, 'running', False)
+            }
+
+            action = actions.get(choice, lambda: print("Invalid choice"))
+            action()
+
         print("Exiting...")
         sleep(2)
             
