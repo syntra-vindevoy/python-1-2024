f3a53546e6966fb64be8fb650d1382ba2a31aadf
Synix1997 <deruyck.sebastien97@gmail.com>
2024-12-12 19:18:45 +0100

    Exercise 8.2 finished

---
+++ Chapter 8/Exercise_8_2.py
@@ -0,0 +1,9 @@
+# Exercise 8.2. There is a string method called count that is similar to the function in Section 8.7.
+# Read the documentation of this method and write an invocation that counts the number of a’s in 'banana'.
+
+word = "banana"
+
+count_letter = word.count("a")
+
+print(count_letter)
+
