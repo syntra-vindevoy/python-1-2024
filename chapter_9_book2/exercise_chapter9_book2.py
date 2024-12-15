bbe00c187b58668e8d62d80c5c0dd895975b15e3
bedoosd <teck_kenneth@hotmail.com>
2024-12-15 11:28:49 +0100

    added full exercise 9.2 and list comprehension

--- chapter_9_book2/exercise_chapter9_book2.py
+++ chapter_9_book2/exercise_chapter9_book2.py
@@ -1,12 +1,15 @@
-from operator import truediv
-
 with open("words.txt", "r") as f:
     words = f.read().split("\n")
+
 def has_no_e(word):
     if word.find("e") == -1: return True
 
-#print (has_no_e("hllo"))
+def check_e_in_list():
+    a = [word for word in words if has_no_e(word)]
+    percentage = len (a) / len(words) * 100
+    print (f"There are {percentage:.2f}% of the words without an 'e' \n {a}")
 
+#check_e_in_list()
 
 def cons_dubble (word):
     for letter in word:
@@ -14,10 +17,8 @@ def cons_dubble (word):
         if len(word) - pos < 6 : return False #stopt wanneer er maar 5 letters over blijven
         if letter == word[pos + 1] and word[pos + 2] == word[pos + 3] and word[pos + 4] == word[pos + 5]: return True
 
-a = []
-for word in words:
-    if len(word) > 3:
-        if cons_dubble(word):
-            a.append(word)
+def check_cons_dubble():
+    a = [word for word in words if cons_dubble(word)]
+    print (f"({len(a)} words found: {a}")
 
-print (f"({len(a)} words found: {a}")
+#check_cons_dubble()
