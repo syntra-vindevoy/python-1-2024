6d07e2203b75ea831de36d8b1e6318100cc1f919
mvdholen <marijnvandenholen@gmail.com>
2024-12-15 22:32:14 +0100

    tkinter dragable and clickable buttons

--- code_snippets/tkinter/movable_buttons.py
+++ code_snippets/tkinter/movable_buttons.py
@@ -5,14 +5,21 @@ class MovableButton(tk.Button):
         super().__init__(master, **kwargs)
         self.bind('<Button-1>', self.on_button_press)
         self.bind('<B1-Motion>', self.on_move)
+        self.bind('<ButtonRelease-1>', self.on_button_release)
+        self._drag_data = {'x': 0, 'y': 0, 'moved': False}
 
     def on_button_press(self, event):
-        self._drag_data = {'x': event.x, 'y': event.y}
+        self._drag_data = {'x': event.x, 'y': event.y, 'moved': False}
 
     def on_move(self, event):
         x = self.winfo_x() - self._drag_data['x'] + event.x
         y = self.winfo_y() - self._drag_data['y'] + event.y
         self.place(x=x, y=y)
+        self._drag_data['moved'] = True
+
+    def on_button_release(self, event):
+        if not self._drag_data['moved']:
+            print(f"{self['text']} action succeeded")
 
 root = tk.Tk()
 root.geometry("400x400")
