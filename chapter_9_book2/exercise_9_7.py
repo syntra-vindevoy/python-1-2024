18736db2540185736b4a7d35604e71aa820c7005
bedoosd <teck_kenneth@hotmail.com>
2024-12-15 10:47:47 +0100

    2 consecutive double letters

---
+++ chapter_9_book2/exercise_9_7.py
@@ -0,0 +1,25 @@
+from operator import truediv
+
+with open("words.txt", "r") as f:
+    words = f.read().split("\n")
+def has_no_e(word):
+    if word.find("e") == -1: return True
+
+#print (has_no_e("hllo"))
+
+
+def cons_dubble (word):
+    for letter in word:
+        pos = word.find(letter)
+        if len(word) - pos < 4 : return False #stopt wanneer er maar 3 letters over blijven
+        if letter == word[pos + 1]:
+            if word[pos + 2] == word[pos + 3]:
+                return True
+
+a = []
+for word in words:
+    if len(word) > 3:
+        if cons_dubble(word):
+            a.append(word)
+
+print (f"({len(a)} words found: {a}")
