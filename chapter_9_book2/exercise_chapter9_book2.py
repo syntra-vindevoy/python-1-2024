83c640bcbf4ba5984abea6933ac198458e30819c
bedoosd <teck_kenneth@hotmail.com>
2024-12-15 19:45:56 +0100

    forbidden words combinations

--- chapter_9_book2/exercise_chapter9_book2.py
+++ chapter_9_book2/exercise_chapter9_book2.py
@@ -22,3 +22,63 @@ def check_cons_dubble():
     print (f"({len(a)} words found: {a}")
 
 #check_cons_dubble()
+
+def uses_none(word, forbidden):
+    for letter in forbidden:
+        if letter in word: return False
+    return True
+
+from itertools import combinations
+import string
+
+def generate_unique_combinations():
+    alphabet = string.ascii_lowercase  # 'abcdefghijklmnopqrstuvwxyz'
+    return [''.join(combo) for combo in combinations(alphabet, 5)]
+
+unique_combinations = generate_unique_combinations()
+#print(f"Aantal combinaties: {len(unique_combinations)}")
+
+
+def search_uses_none(forbidden):
+    n = 0
+    for word in words:
+        if uses_none(word, (forbidden)): n += 1
+    #print (f"total number of words: {len(words)}, number of forbidden words: {n}")
+    return n
+#search_uses_none ()
+
+def calculate_used_letters():
+    alphabet = string.ascii_lowercase
+    letter_counts = {letter: 0 for letter in alphabet}
+
+    for word in words:
+        for letter in alphabet:
+            if letter in word:
+                letter_counts[letter] += 1
+
+    sorted_letters = sorted(letter_counts.items(), key=lambda x: x[1])
+    return sorted_letters
+
+#most used letters:
+#search_uses_none("qjxzw")
+#least used letters:
+#search_uses_none("esiar")
+def search_all_combinations():
+    result_max = 0
+    result_min = 100000
+    combination_max = ""
+    combination_min = ""
+    count = 0
+    for n in unique_combinations:
+        count += 1
+        result = search_uses_none(n)
+        if result > result_max:
+            result_max = result
+            combination_max = n
+
+        if result < result_min:
+            result_min = result
+            combination_min = n
+        print (f"{count / len(unique_combinations) * 100:.2f}%") if count % 100 == 0 else None
+    print (f"{combination_max} = {result_max} and {combination_min} = {result_min}")
+#search_all_combinations()
