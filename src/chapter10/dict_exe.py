111effdc8bc6d712f045a65a18963912aa2e256d
Benoit Goethals <benoit.goethals@gmail.com>
2024-12-18 09:43:33 +0100

    Add new examples for dictionaries, sets, and strings
    Added scripts demonstrating dictionary, set, and string operations to respective chapters. Also updated file structure by renaming `ex_10_9.py` and adjusting a file path for consistency in Chapter 9.

---
+++ src/chapter10/dict_exe.py
@@ -0,0 +1,34 @@
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+print(dict1['a'])
+print(dict1.get('a'))
+print(dict1.get('d'))
+print(dict1.get('d', 4))
+dict1['a'] = 10
+print(dict1)
+dict1['d'] = 4
+print(dict1)
+dict1.update({'e': 5, 'f': 6})
+
+print(dict1['a'])
+
+del dict1['a']
+
+print(dict1)
+
+dict1.popitem()
+print(dict1)
+
+if 'b' in dict1:
+    print('b in dict1')
+
+
+for k,v in dict1.items():
+    print(k,v)
+
+new_dict = dict(dict1)
+print(new_dict)
+
+
+dicct3 = {'a': 10, 'b': 9, 'c': 6}
+dicct3.update(dict1)
+print(dicct3)
