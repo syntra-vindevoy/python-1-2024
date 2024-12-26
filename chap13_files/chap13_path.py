e78cc4fe4a7030d9b97b085622e327b42e66b09c
Thomas <thomas.meersschaut@gmail.com>
2024-12-26 19:41:43 +0100

    Add chap13_path.py for file and path operations examples
    This new script demonstrates file writing and path handling using `pathlib` and `os`. It includes examples of creating and writing to files, navigating directories, and constructing paths. This serves as a foundation for chapter 13 exercises.

---
+++ chap13_files/chap13_path.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+names = ["Thomas\n", "Meersschaut\n"]
+current_dir = Path(__file__).parent.absolute()      #Directory of file
+#cwd = os.getcwd()
+
+#with open(f"{cwd}/outputpath.txt", "w") as file:
+#    file.writelines(names)
+
+with open(f"{current_dir}/outputpath2.txt", "w") as file:
+    file.writelines(names)
+
+print(current_dir)
+
+config_dir = current_dir.parent.joinpath("config", "settings.ini")
+print(config_dir)
+
+os.path.pathsep
+windows_path = r"c:\temp\output.txt"        #raw string
