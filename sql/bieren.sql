23ff84bfb5ca9a2590c1ec91192c3bd7396977bb
Benoit Goethals <Benoit.goethals@gmail.com>
2025-01-05 12:35:48 +0100

    Add new queries for beer and brewer data analysis
    Several SQL queries were added to retrieve detailed information about beers, brewers, and their relationships. These include aggregations, distinct selections, and filtered groupings by alcohol levels. Minor syntax corrections were also made for consistency.

--- sql/bieren.sql
+++ sql/bieren.sql
@@ -13,7 +13,7 @@ ORDER BY b.naam;
 
 create unique index  id_bieren_naam on bieren(naam) ;
 
-drop index id_bieren_naam
+drop index id_bieren_naam;
 
 
 SELECT
@@ -30,3 +30,47 @@ FROM
 
     and alcohol = (select max(alcohol) from bieren where alcohol < 15)
 ORDER BY alcohol desc;
+
+
+select distinct bieren.naam, bieren.alcohol from bieren;
+
+
+SELECT
+brouwernaam, b.naam
+FROM
+    bieren b
+        INNER JOIN
+    brouwers br ON br.brouwernr = b.brouwernr
+group by brouwernaam, b.naam;
+
+
+
+SELECT
+    b.naam AS bier,
+    br.brouwernaam AS brouwer,
+    s.soort
+FROM
+    bieren b
+        INNER JOIN
+    brouwers br ON br.brouwernr = b.brouwernr
+        INNER JOIN
+    soorten s ON b.soortnr = s.soortnr;
+
+
+SELECT br.brouwernaam, COUNT(b.biernr)
+FROM public.brouwers AS br
+INNER JOIN public.bieren AS b
+ON br.brouwernr = b.brouwernr
+GROUP BY br.brouwernaam;
+
+SELECT br.brouwernaam, COUNT(b.biernr)
+FROM public.brouwers AS br
+INNER JOIN public.bieren AS b
+ON br.brouwernr = b.brouwernr
+WHERE b.alcohol > 6 AND b.alcohol < 10
+GROUP BY br.brouwernaam
+
+
+
+
+
