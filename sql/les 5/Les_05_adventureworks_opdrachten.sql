a26b0478f69df06d0b58ca0b0cc0f238b75f672f
Benoit Goethals <Benoit.goethals@gmail.com>
2025-01-08 14:09:34 +0100

    Add SQL examples and exercises for SELECT, WHERE, and JOIN.
    This commit introduces SQL scripts with examples and exercises for working with `SELECT`, `WHERE`, `GROUP BY`, `HAVING`, and various types of `JOIN` clauses. These queries include filtering conditions, sorting, row limits, and aggregations, covering scenarios from both "bieren" and "adventureworks" databases. The scripts also include comments and notes on SQL standards, enhancing learning and compatibility.

---
+++ sql/les 5/Les_05_adventureworks_opdrachten.sql
@@ -0,0 +1,280 @@
+-- *** SELECT *** 
+
+
+
+
+
+
+-- Toon alle gegevens uit de tabel person.person
+
+
+
+-- Toon enkel PersonType, Title, FirstName, LastName
+
+
+
+-- Geef de volledig inhoud v/d tabel CountryRegion
+
+
+-- Uit de tabel Product, geef de naam van het product, het productnummer, de kleur,
+-- de voorraaddrempel en de aanduiding of het product geproduceerd of aangekocht is.
+
+
+
+
+-- *** WHERE ***
+-- Toon alle gegevens uit de tabel persoon voor de persoon met id 123
+
+
+
+
+-- Selecteer alle staten van de U.S.
+
+
+
+
+-- Selecteer alle staten, behalve die in de U.S.
+
+
+
+
+-- Selecteer alle adressen uit New York & Los Angeles op twee verschillende manieren
+
+
+
+
+
+
+
+
+-- Selecteer alle producten waarvan de voorraaddrempel tussen 10 en 100 ligt 
+-- Doe dit op twee verschillende manieren
+
+
+
+
+
+
+
+
+-- Selecteer alle producten, sorteer ze op kost in aflopende volgorde
+-- en op naam in oplopende volgorde (alfabetisch)
+
+
+
+
+-- Tel het aantal producten
+
+
+
+-- Zoek alle staten waarvan de naam begint met "New".
+
+
+
+
+-- Is dit case sensitief ?
+
+
+
+
+-- Hoe kleine en grote letters mee hebben?
+
+
+
+
+
+
+
+
+
+-- *** GROUP BY & HAVING ***
+
+
+
+
+
+
+
+-- Wat is het min- & maximum uurloon dat de winkel ooit uitbetaalde aan een werknemer?
+
+
+
+
+-- Tel, per achternaam, het aantal personen in de Person tabel. 
+-- Sorteer aflopend op aantal en vervolgens alfabetisch op naam?
+
+
+
+
+
+
+-- Tel het aantal producten op basis v/d eerste letter van hun naam. Sorteer alfabetisch.
+
+
+
+
+
+
+
+
+
+
+
+
+-- Lijst het aantal adressen op per postcode. 
+-- Geef enkel de postcodes weer met meer dan 5 adressen. 
+-- Sorteer aflopend op basis van aantal adressen.
+
+
+
+
+
+
+
+-- Tel het aantal productbeschrijvingen op basis v/d lengte. 
+-- Neem enkel producten mee met een productbeschrijving die langer is dan 10 tekens. 
+-- Toon enkel de rijen met meer dan 5 beschrijvingen. 
+-- Sorteer oplopend op basis van lengte v/d beschrijving.
+
+
+
+
+
+
+
+
+-- Maak een lijst van alle adressen. Het resultaat bevat twee kolommen:
+-- addressID en full_address. Deze laatste bevat zowel de straatnaam, postcode en stad.
+
+
+
+
+
+
+
+
+-- *** JOINS *** --
+-- Maak een lijst van producten (zowel productnaam als –nummer), 
+-- waar ze in de stock terug te vinden zijn (compartiment en container)
+-- en hoeveel er nog in stock zijn. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Van welke producten is er geen stock meer: geef het productID, -naam en nummer en de hoeveelheid in stock mee. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- ALL vs ANY
+
