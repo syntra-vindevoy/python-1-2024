08e1e3b56fbe8279903e6335fdf8a90301478247
Thomas <thomas.meersschaut@gmail.com>
2025-01-09 21:35:32 +0100

    Add script to invert a dictionary and sort by keys
    This script includes a function to invert a dictionary and utilizes `SortedDict` from the `sortedcontainers` library to sort rankings by keys. It also demonstrates the functionality with a sample dataset of football teams and their points.

---
+++ chap10_dict/chap11.py
@@ -0,0 +1,16 @@
+from sortedcontainers import SortedDict
+
+def invert_dict(d):
+    inverse = dict()
+    for key in d:
+        val = d[key]
+        if val not in inverse:
+            inverse[val] = [key]
+        else:
+            inverse[val].append(key)
+    return inverse
+
+points = {"Anderlecht" : 33, "Club Brugge" : 41, "Genk" : 42, "Antwerp" : 32, "Union" : 33}
+rankings = SortedDict(invert_dict(points))
+
+print(rankings)
