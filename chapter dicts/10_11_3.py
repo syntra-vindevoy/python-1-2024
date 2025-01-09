dd2486c5d9a477324f5bf0ed16f1738dd18526bb
bedoosd <teck_kenneth@hotmail.com>
2025-01-09 21:02:41 +0100

    exercise boek 3 10_11_3

--- chapter dicts/10_11_3.py
+++ chapter dicts/10_11_3.py
@@ -5,10 +5,27 @@ Write a function named has_duplicates that takes a sequence –
 like a list or string – as a parameter and returns True if there is any element that appears in the sequence more than once.
 """
 
-def has_duplicates(word_list):
-    d = {}
-    for word in word_list:
-        d [word] = d.get(word, 0) + 1
-        if d[word] > 1: return True
 
-print(has_duplicates(["hallo", "hal", "aj", "jh", "ha"]))
+def has_duplicates(word):
+    d = {}
+    for letter in word:
+        d [letter] = d.get(letter, 0) + 1
+        if d[letter] > 1: return True
+
+print(has_duplicates("hallo"))
+
+with open ("words.txt", "r") as f:
+    words = f.read().splitlines()
+    words = [word for word in words if len(word) == len(set(word))]
+    word_lenghts = {len(word): word for word in words}
+print(sorted(word_lenghts, reverse=True)[0])
+print (word_lenghts[max(word_lenghts.keys())])
+
+def longest_word():
+    with open ("words.txt", "r") as f:
+        words = f.read().splitlines()
+        words2 = {}
+        for word in words:
+            words2[word] = len(word)
+        print (words2)
+longest_word()
