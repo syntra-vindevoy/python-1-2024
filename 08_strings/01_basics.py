49a50637424f344990766c5528207713667905d0
Thomas <thomas.meersschaut@gmail.com>
2025-01-11 15:25:10 +0100

    Add basic string operations example script
    This script demonstrates fundamental string operations, including indexing, length calculation, and slicing. It serves as an introductory example for handling strings in Python.

---
+++ 08_strings/01_basics.py
@@ -0,0 +1,13 @@
+
+name = "Thomas"
+n = len(name)
+letter = name[1]
+
+print("Name is:", name)
+print("The length of the name is", n, "characters")
+print("The 1st letter is:", name[0])
+print("The 2nd letter is:", letter)
+print("The last letter is:", name[-1])
+print("Index -1 starts counting from the end of the string.")
+print("The 2nd to last letter is:", name[-2])
+print("The 3rd to last letter is:", name[-3])
