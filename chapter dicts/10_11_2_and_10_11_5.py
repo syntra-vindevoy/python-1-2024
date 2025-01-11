5e372b12df5548009eae517e636bb11481846f24
bedoosd <teck_kenneth@hotmail.com>
2025-01-11 10:10:03 +0100

    exercise boek 3 added .get in 10.11.5

--- chapter dicts/10_11_2_and_10_11_5.py
+++ chapter dicts/10_11_2_and_10_11_5.py
@@ -6,17 +6,24 @@ def value_counts(word):
 
 value_counts("hallo")
 
+# def merge_counts():
+#     first = value_counts("hallo")
+#     second = value_counts("okidoki")
+#     merge = first.copy()
+#     for letter in second:
+#         if letter in first:
+#             merge[letter] += second.get(letter)
+#         else: merge[letter] = second.get(letter)
+#     print (merge)
+
 def merge_counts():
     first = value_counts("hallo")
     second = value_counts("okidoki")
     merge = first.copy()
-    print (second)
-    print (second.values())
     for letter in second:
-        if letter in first:
-            merge[letter] += second.get(letter)
-        else: merge[letter] = second.get(letter)
-    print (merge)
+        merge[letter] = merge.get(letter, 0) + second[letter]
+    print(merge)
+    return merge
 
 
 merge_counts()
