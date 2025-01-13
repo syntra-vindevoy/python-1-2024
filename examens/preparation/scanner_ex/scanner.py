f4f8815eabd09d173a63183accc07e52ccc89677
Benoit Goethals <Benoit.goethals@gmail.com>
2025-01-13 13:56:26 +0100

    Add scanner utility to analyze Python files for keywords
    This commit introduces a script to recursively scan directories for Python files and extract keywords like 'def', 'class', and control structures. The utility organizes occurrences by word, file, and paragraph, aiding in file content analysis.

---
+++ examens/preparation/scanner_ex/scanner.py
@@ -0,0 +1,57 @@
+"""
+scan files for words
+"""
+import pathlib
+from pathlib import Path
+
+
+def main():
+    print("Scanner")
+    dict_paras = {}
+    word_list = ["def", "class", "if", "for", "while"]
+    result = scan_directory(Path(__file__).parent.parent, "py")
+    for file in result:
+        d = scan_file(file, word_list=["def", "class", "if", "for", "while"])
+        for key in d:
+            dict_paras[key] = dict_paras.get(key, []) + d[key]
+    for key in dict_paras:
+        print(key)
+        for file, para, index in dict_paras[key]:
+            print(f"\t{file} : {para} ({index})")
+
+
+def scan_directory(dir_path, suffix) -> [pathlib.Path]:
+    filenames = []
+    for f in Path(dir_path).iterdir():
+        if f.is_file() and f.suffix[1:] == suffix:
+            filenames.append(f)
+        elif f.is_dir():
+            filenames.extend(scan_directory(f.resolve(), suffix))
+    return filenames
+
+
+def scan_file(path: Path, word_list: list):
+    dict_words = {}
+    with open(path, 'r') as file_c:
+        content = file_c.read()
+        # seperate in para
+        paragraphs = [para.strip() for para in content.split("\n\n") if para.strip()]
+
+        # search for words
+        for para in paragraphs:
+            for word in word_list:
+                if word in para:
+                    if word not in dict_words:
+                        dict_words[word] = [(path, para, para.index(word))]
+                    else:
+                        dict_words[word].append((path, para, para.index(word)))
+                # dict_words[word] = dict_words.get(word, []).append((path, para,para.index(word)))
+    return dict_words
+
+
+def make_summary():
+    print("Make summary")
+
+
+if __name__ == '__main__':
+    main()
