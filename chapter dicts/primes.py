b7f3bd6d29a2489fefff7291157b75b1afaea1df
bedoosd <teck_kenneth@hotmail.com>
2025-01-14 21:45:45 +0100

    primes with list method, removed line

--- chapter dicts/primes.py
+++ chapter dicts/primes.py
@@ -3,13 +3,13 @@ def primes (number):
     n = 11
     while len(prime_list) < number:
         prime_check = True
-        for i in prime_list[3:]:
+        for i in prime_list[1:]:
             if n % i == 0:
                 prime_check = False
                 break
         if prime_check: prime_list.append(n)
         n += 2
-        while n % 5 == 0 or n % 3 == 0: n += 2
+
     return prime_list
 
 print (primes(1000))
