0dd796daa9f8a5c645ba33d5effb8726bf3a95f5
bedoosd <teck_kenneth@hotmail.com>
2025-01-15 19:50:04 +0100

    added stop at sqrt of n and added time counter

--- chapter dicts/primes.py
+++ chapter dicts/primes.py
@@ -1,15 +1,23 @@
+from time import perf_counter
+
 def primes (number):
-    prime_list = [2,3,5,7,]
-    n = 11
+    prime_list = [2,]
+    n = 3
     while len(prime_list) < number:
         prime_check = True
         for i in prime_list[1:]:
+            if i * i > n: break #stops sequence at sqrt of n, no need to continue checking higher values
             if n % i == 0:
                 prime_check = False
                 break
         if prime_check: prime_list.append(n)
         n += 2
-
     return prime_list
 
-print (primes(1000))
+if __name__ == '__main__':
+    start = perf_counter()
+    n = 10000
+    primes_list = primes(n)
+    print (primes_list)
+    stop = perf_counter()
+    print (f"{len(primes_list)} primes found in {stop - start} seconds")
