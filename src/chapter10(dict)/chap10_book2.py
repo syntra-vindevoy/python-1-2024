fd23bc42fa4d6e103b2dcd2d0bf1f2ccc6e350f7
Benoit Goethals <Benoit.goethals@gmail.com>
2025-01-24 11:38:27 +0100

    Add new word rotation and keyword matching functionalities
    Integrated a `rotate_word` function and pair-finding logic in chap10, enabling word manipulation tasks. Added new script `list_ex_vowels.py` for keyword matching in text and vowel pair detection, enhancing text processing capabilities. Minor adjustments made in `list_ex.py` to support extensions.

--- src/chapter10(dict)/chap10_book2.py
+++ src/chapter10(dict)/chap10_book2.py
@@ -161,6 +161,37 @@ def dummy_homophone_check(word1, word2):
     return word1.lower() == word2.lower()
 
 
+from string import ascii_lowercase
+
+
+def rotate_word(word, rotation):
+    word = word.lower()
+    alphabet = ascii_lowercase * 2
+    # alphabet_list = list(alphabet)
+    new_word = ""
+    for letter in word:
+        i = (alphabet.index(letter))
+        new_word += alphabet[i + rotation]
+    return new_word
+
+
+def find_pairs():
+    with open("words.txt", "r") as f:
+        words_dict = {word: 1 for word in f.read().splitlines()}
+        pairs_list = []
+        print(words_dict)
+    for word in words_dict:
+        new_word = rotate_word(word, 1)
+        if new_word in words_dict: pairs_list.append((word, new_word))
+
+    return pairs_list
+
+
+
+
+
+
+
 # Example usage
 if __name__ == "__main__":
     # Example word list
