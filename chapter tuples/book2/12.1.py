f4f7d788946257e389a4be20c6bbdad8603f1a1b
bedoosd <teck_kenneth@hotmail.com>
2025-01-26 20:01:44 +0100

    exercise 12.1 book 2

---
+++ chapter tuples/book2/12.1.py
@@ -0,0 +1,20 @@
+"""Exercise 12.1. Write a function called most_frequent that takes a string and prints the let-
+ters in decreasing order of frequency. Find text samples from several different languages and see
+how letter frequency varies between languages"""
+
+def letter_frequency(file_name):
+    with open (file_name, "r") as f:
+        words = f.read().split()
+    letters_count = {}
+    for item in words:
+        for i in item:
+            if i.isalpha():
+                i = i.lower()
+                letters_count[i] = letters_count.get(i, 0) + 1
+
+    letters_count = sorted(letters_count.items(), key=lambda item: item[1],reverse=True)
+    letters = [letter for letter, count in letters_count]
+    return letters
+print (f"Dutch: {letter_frequency("text_nl.txt")}")
+print(f"English: {letter_frequency("text_en.txt")}")
+print(f"French: {letter_frequency("text_fr.txt")}")
