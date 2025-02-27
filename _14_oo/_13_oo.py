f85ea0e55acaeddc5c4c8fc1ab34ce9cc71907c9
Thomas <thomas.meersschaut@gmail.com>
2025-02-27 19:52:36 +0100

    Add Contact and Product classes with corresponding list handlers
    Introduce `Contact` and `ContactList` classes for managing contacts. Add `Product` and `ProductList` classes, implementing type-checking in `ProductList.append` to enforce only `Product` objects can be added. Include example usage for testing purposes.

---
+++ _14_oo/_13_oo.py
@@ -0,0 +1,44 @@
+class Contact:
+    pass
+
+class ContactList:
+    def __init__(self):
+        self.contacts = []
+
+
+l = ContactList()
+
+c = Contact()
+l.contacts.append(c)
+
+
+
+class Product:
+    def __init__(self, name):
+        self.name = name
+
+
+class ProductList(list):
+    def __init__(self):
+        super().__init__()
+
+    def append(self, product: Product):
+        if type(product) != Product:
+            raise ValueError("ProductList.append expects a Product")
+        super().append(product)
+
+
+l2 = ProductList()
+p = Product("Thomas")
+
+l2.append(p)
+print(len(l2))
+
+my_product: Product = l2[0]
+
+
+print(l2[0].name)
+
+p1 = Product("John")
+l2.append(p1)
+l2.append(c)
