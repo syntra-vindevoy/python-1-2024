c6f28f86f9d4e0785e98c1a07d51d7d115640a2d
bedoosd <teck_kenneth@hotmail.com>
2025-03-18 19:25:14 +0100

    correctie in de les

--- object_oriented/cards/card_methods.py
+++
@@ -1,5 +0,0 @@
-from printdeck import Printdeck
-from draw_card import DrawCard
-
-New_deck = Printdeck()
-Draw_one = DrawCard()
--- object_oriented/cards/carddeck.py
+++ object_oriented/cards/carddeck.py
@@ -1,3 +1,5 @@
+import random
+
 
 class Carddeck:
     numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
@@ -5,6 +7,40 @@ class Carddeck:
     def __init__(self):
         super().__init__()
         self.deck = []
+        self.start()
+
+    def start(self):
+        self.deck = []
         for suit in self.suits:
             for number in self.numbers:
                 self.deck.append((suit, number))
+        self.shuffle()
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def show(self):
+        print (self.deck)
+
+    def draw(self):
+        if len(self.deck) == 0:
+            print ("No more cards to draw")
+            return None
+        else:
+            card = self.deck[0]
+            self.deck = self.deck[1:]
+            return card
+    @property
+    def remaining(self):
+        return len(self.deck)
+
+c = Carddeck()
+c.show()
+for i in range(3):
+
+    d = c.draw()
+    print (d)
+print(c.remaining)
+
+
+
