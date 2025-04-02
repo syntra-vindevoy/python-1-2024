0dc32dda8ac149678c00b7ca11258e3696448818
Benoit Goethals <Benoit.goethals@gmail.com>
2025-04-02 13:54:00 +0200

    Add object-oriented DI framework with single/multi-container
    Implemented an object-oriented dependency injection framework, supporting both single-container and multi-container configurations. Added services for user, authentication, and photo operations, along with service-specific logging. Configured container classes and resources for modular and scalable DI setups.

---
+++ src/OO/injection/containter_single/__main__.py
@@ -0,0 +1,30 @@
+"""Main module."""
+
+import sys
+
+from dependency_injector.wiring import Provide, inject
+
+from .containers import Container
+from .services import UserService, AuthService, PhotoService
+
+
+@inject
+def main(
+        email: str,
+        password: str,
+        photo: str,
+        user_service: UserService = Provide[Container.user_service],
+        auth_service: AuthService = Provide[Container.auth_service],
+        photo_service: PhotoService = Provide[Container.photo_service],
+) -> None:
+    user = user_service.get_user(email)
+    auth_service.authenticate(user, password)
+    photo_service.upload_photo(user, photo)
+
+
+if __name__ == "__main__":
+    container = Container()
+    container.init_resources()
+    container.wire(modules=[__name__])
+
+    main(*sys.argv[1:])
---
+++ src/OO/injection/multi-container/__main__.py
@@ -0,0 +1,30 @@
+"""Main module."""
+
+import sys
+
+from dependency_injector.wiring import Provide, inject
+
+from .containers import Application
+from .services import UserService, AuthService, PhotoService
+
+
+@inject
+def main(
+        email: str,
+        password: str,
+        photo: str,
+        user_service: UserService = Provide[Application.services.user],
+        auth_service: AuthService = Provide[Application.services.auth],
+        photo_service: PhotoService = Provide[Application.services.photo],
+) -> None:
+    user = user_service.get_user(email)
+    auth_service.authenticate(user, password)
+    photo_service.upload_photo(user, photo)
+
+
+if __name__ == "__main__":
+    application = Application()
+    application.core.init_resources()
+    application.wire(modules=[__name__])
+
+    main(*sys.argv[1:])
